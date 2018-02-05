f031c5fdc8d6978895d09369e65aa5c0bb028e7b
James Waples <jamwaffles@gmail.com>
2018-02-05 21:42:40 +0000

    Add 8BPP image drawing func to trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
 pub trait Drawing {
-	fn set_pixel(&mut self, x: u8, y: u8, value: u8);
+	fn set_pixel(&mut self, x: u32, y: u32, value: u8);
+	fn draw_image_8bpp(&mut self, bytes: &[u8], w: u32, h: u32);
 }
