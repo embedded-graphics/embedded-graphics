4249f075282d90a623253b20fc7da52e4a5911cc
James Waples <jamwaffles@gmail.com>
2018-02-05 22:08:15 +0000

    Support image positioning

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@
 
 pub trait Drawing {
 	fn set_pixel(&mut self, x: u32, y: u32, value: u8);
-	fn draw_image_8bpp(&mut self, bytes: &[u8], w: u32, h: u32);
+	fn draw_image_8bpp(&mut self, image: &[u8], w: u32, h: u32, x: u32, y: u32);
 }
