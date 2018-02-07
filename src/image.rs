e8070ac88beb4fa9e14f1f73fc6cc4b6921a944e
James Waples <jamwaffles@gmail.com>
2018-02-07 21:00:18 +0000

    Add 1BPP image type

--- src/image.rs
+++ src/image.rs
@@ -3,3 +3,9 @@ pub struct Image8BPP<'a> {
 	pub height: u32,
 	pub imagedata: &'a [u8],
 }
+
+pub struct Image1BPP<'a> {
+	pub width: u32,
+	pub height: u32,
+	pub imagedata: &'a [u8],
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ pub mod image;
 
 pub trait Drawing {
 	fn set_pixel(&mut self, x: u32, y: u32, value: u8);
-	fn draw_image_8bpp(&mut self, image: image::Image8BPP, x: u32, y: u32);
+	fn draw_image_8bpp(&mut self, image: &image::Image8BPP, x: u32, y: u32);
+	fn draw_image_1bpp(&mut self, image: &image::Image1BPP, x: u32, y: u32);
 }
