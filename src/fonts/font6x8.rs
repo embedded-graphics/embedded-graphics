37bd90344d4153905d686f2de4bcc9b83445b0fa
James Waples <james@cambridge-intelligence.com>
2018-02-08 14:26:56 +0000

    WIP (again) fonts
    Each char in the bitmap is 8 bits wide

--- src/fonts/font6x8.rs
+++ src/fonts/font6x8.rs
@@ -4,48 +4,68 @@ use super::super::image::{ Image1BPP };
 const font: &[u8] = include_bytes!("../../font6x8_1bpp.raw");
 const font_widths: &[u8] = include_bytes!("../../font6x8_widths.raw");
 const char_height: u8 = 8;
+const char_width: u8 = 6;
 const first_charcode: u8 = 32;		// A space
 const image_width: u8 = 192;
+const max_stride: u8 = 1;	// 2 bytes is max glyph width
+const byte_width: u8 = image_width / 8;
 
 pub struct Font6x8 {}
 
 impl Font6x8 {}
 
-impl Font for Font6x8 {
-	fn render_str(&self, chars: &str) -> Image1BPP {
-		let (x, y, width) = char_pixel_info();
+impl Font6x8 {
+	fn render_str(text: &str) {
+		for c in text.chars() {
+			let byte_offset = (c as u8 - first_charcode);
 
-		let ptr = font.as_ptr();
+			println!("charcode = {}, byte = {} {}", (c as u8 - first_charcode), byte_offset, byte_width);
 
+			let bytes = [
+				font[(byte_offset) as usize],
+				font[(byte_offset + byte_width) as usize],
+				font[(byte_offset + byte_width * 2) as usize],
+				font[(byte_offset + byte_width * 3) as usize],
+				font[(byte_offset + byte_width * 4) as usize],
+				font[(byte_offset + byte_width * 5) as usize],
+				font[(byte_offset + byte_width * 6) as usize],
+				font[(byte_offset + byte_width * 7) as usize],
+			];
 
-
-		Image1BPP {
-			width,
-			height: char_height,
-			imagedata: ;
+			println!("{:?}", bytes);
 		}
 	}
 }
 
-// Offset x, offset y, char block width
-fn char_pixel_info(c: char) -> (u8, u8, u8) {
-	let char_index = (first_charcode - c as u8) as usize;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-	// As if all the chars were on one line, compute the char X offset for a variable width font
-	let cum_offset: u32 = font_widths
-		.iter()
-		.take(char_index)
-		.fold(0u32, |accum, &width| accum + width as u32);
-
-	let char_width = font_widths[char_index];
-
-	let row_offset = (cum_offset / image_width as u32) as u8;
-
-	let pixel_offset = cum_offset - (row_offset * image_width) as u32;
-
-	(
-		pixel_offset as u8,
-		row_offset * char_height,
-		char_width
-	)
+    #[test]
+    fn render_str() {
+        Font6x8::render_str("!");
+    }
 }
+
+// Offset x, offset y, char block width
+// fn char_pixel_info(c: char) -> (u8, u8, u8) {
+// 	let char_index = (c as u8 - first_charcode) as usize;
+
+// 	// As if all the chars were on one line, compute the char X offset for a variable width font
+// 	let cum_offset: u32 = font_widths
+// 		.iter()
+// 		.take(char_index)
+// 		.fold(0u32, |accum, &width| accum + width as u32);
+
+// 	let char_width = font_widths[char_index];
+
+// 	let row_offset = (cum_offset / image_width as u32) as u8;
+
+// 	let pixel_offset = cum_offset - (row_offset * image_width) as u32;
+
+// 	(
+// 		pixel_offset as u8,
+// 		row_offset * char_height,
+// 		char_width
+// 	)
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+// #![no_std]
 
 pub mod image;
 pub mod fonts;
