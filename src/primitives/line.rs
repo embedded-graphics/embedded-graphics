7e3b5bc6de6d7407e5d28332da0dfbb3871163fe
Byron Wasti <byron.wasti@gmail.com>
2018-04-05 14:18:53 -0400

    Fix formatting

--- src/primitives/line.rs
+++ src/primitives/line.rs
@@ -29,54 +29,51 @@ impl<'a> IntoIterator for &'a Line {
     type IntoIter = LineIterator<'a>;
 
     fn into_iter(self) -> Self::IntoIter {
-        let &Line { start:(x0, y0), end:(x1, y1), .. } = self;
+        let &Line {
+            start: (x0, y0),
+            end: (x1, y1),
+            ..
+        } = self;
 
         // Find out if our line is steep or shallow
         let is_steep = (y1 as i32 - y0 as i32).abs() > (x1 as i32 - x0 as i32).abs();
 
         // Determine if endpoints should be switched
         // based on the "quick" direction
-        let (x0, y0, x1, y1) = 
-            if is_steep {
-                if y0 > y1 {
-                    (x1, y1, x0, y0)
-                } else {
-                    (x0, y0, x1, y1)
-                }
+        let (x0, y0, x1, y1) = if is_steep {
+            if y0 > y1 {
+                (x1, y1, x0, y0)
             } else {
-                if x0 > x1 {
-                    (x1, y1, x0, y0)
-                } else {
-                    (x0, y0, x1, y1)
-                }
-            };
+                (x0, y0, x1, y1)
+            }
+        } else {
+            if x0 > x1 {
+                (x1, y1, x0, y0)
+            } else {
+                (x0, y0, x1, y1)
+            }
+        };
 
         // Setup our pre-calculated values
         let (dquick, mut dslow) = if is_steep {
-                (y1 as i32 - y0 as i32,
-                 x1 as i32 - x0 as i32)
-            } else {
-                (x1 as i32 - x0 as i32,
-                 y1 as i32 - y0 as i32)
-            };
+            (y1 as i32 - y0 as i32, x1 as i32 - x0 as i32)
+        } else {
+            (x1 as i32 - x0 as i32, y1 as i32 - y0 as i32)
+        };
 
         // Determine how we should increment the slow direction
-        let increment = if dslow < 0 { 
-                dslow = -dslow;
-                -1
-            } else { 
-                1 
-            };
+        let increment = if dslow < 0 {
+            dslow = -dslow;
+            -1
+        } else {
+            1
+        };
 
         // Compute the default error
-        let error = 2*dslow - dquick;
+        let error = 2 * dslow - dquick;
 
         // Set our inital quick & slow
-        let (quick, slow, end) = if is_steep {
-                (y0, x0, y1)
-            } else {
-                (x0, y0, x1)
-            };
+        let (quick, slow, end) = if is_steep { (y0, x0, y1) } else { (x0, y0, x1) };
 
         LineIterator {
             line: self,
@@ -116,23 +113,23 @@ impl<'a> Iterator for LineIterator<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.quick == self.end {
-            return None
+            return None;
         }
 
         // Get the next point
         let &Line { color, .. } = self.line;
         let coord = if self.is_steep {
-                (self.slow, self.quick)
-            } else {
-                (self.quick, self.slow)
-            };
+            (self.slow, self.quick)
+        } else {
+            (self.quick, self.slow)
+        };
 
         // Update error and increment slow direction
         if self.error > 0 {
             self.slow = (self.slow as i32 + self.increment) as u32;
-            self.error -= 2*self.dquick;
+            self.error -= 2 * self.dquick;
         }
-        self.error += 2*self.dslow;
+        self.error += 2 * self.dslow;
 
         // Increment fast direction
         self.quick += 1;
