617b3d299da93dd04c541304cf36441b96ae8920
Byron Wasti <byron.wasti@gmail.com>
2018-05-03 12:08:30 -0400

    Add basic simulator support

---
+++ simulator/src/main.rs
@@ -0,0 +1,41 @@
+extern crate embedded_graphics;
+extern crate simulator;
+
+use std::time::Duration;
+use std::thread;
+
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, Line};
+use embedded_graphics::image::Image1BPP;
+use embedded_graphics::fonts::Font6x8;
+
+use simulator::Display;
+
+fn main() {
+    let mut display = Display::new();
+
+    // Outline
+    display.draw(Circle::new((64, 64), 63, 1).into_iter());
+
+    // Clock hands
+    display.draw(Line::new((64, 64), (0, 64), 1).into_iter());
+    display.draw(Line::new((64, 64), (80, 80), 1).into_iter());
+
+    display.draw(Font6x8::render_str("Hello World!").translate((5, 50)).into_iter());
+
+
+    /*
+    let bpp = Image1BPP::new(include_bytes!("../data/face_1bpp.raw"), 120, 120).translate((0, 0));
+    display.draw(bpp.into_iter());
+    */
+
+    loop {
+        let end = display.run_once();
+
+        if end {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(200));
+    }
+}
