87d29583e966744cd12b87efe2d1cb5993eefd38
Byron Wasti <byron.wasti@gmail.com>
2018-05-03 12:08:30 -0400

    Add basic simulator support

---
+++ simulator/src/lib.rs
@@ -0,0 +1,85 @@
+extern crate sdl2;
+extern crate embedded_graphics;
+
+use embedded_graphics::prelude::*;
+use embedded_graphics::Drawing;
+use embedded_graphics::drawable::Pixel;
+
+use sdl2::render;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+
+const DISPLAY_SIZE: usize = 128;
+
+pub struct Display {
+    pixels: [[bool; DISPLAY_SIZE]; DISPLAY_SIZE],
+    canvas: render::Canvas<sdl2::video::Window>,
+    event_pump: sdl2::EventPump,
+}
+
+impl Display {
+    pub fn new() -> Self {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+
+        let window = video_subsystem.window("graphics-emulator", DISPLAY_SIZE as u32, DISPLAY_SIZE as u32)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let mut canvas = window.into_canvas().build().unwrap();
+        let mut event_pump = sdl_context.event_pump().unwrap();
+
+        Self {
+            pixels: [[false; DISPLAY_SIZE]; DISPLAY_SIZE],
+            canvas,
+            event_pump,
+        }
+    }
+
+    pub fn run_once(&mut self) -> bool {
+        // Handle events
+        for event in self.event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | 
+                Event::KeyDown { keycode: 
+                    Some(Keycode::Escape), .. } => {
+                    return true;
+                },
+                _ => {}
+            }
+        }
+
+        self.canvas.set_draw_color(Color::RGB(255, 255, 255));
+        self.canvas.clear();
+
+        self.canvas.set_draw_color(Color::RGB(0, 0, 0));
+        for (y, line) in self.pixels.iter().enumerate() {
+            for (x, value) in line.iter().enumerate() {
+                if *value {
+                    let x = x as i32;
+                    let y = y as i32;
+                    self.canvas.fill_rect(Rect::new(x, y, 1, 1)).unwrap();
+                }
+            }
+        }
+        
+        self.canvas.present();
+        false
+    }
+}
+
+impl Drawing for Display {
+    fn draw<T>(&mut self, item_pixels: T)
+    where T: Iterator<Item = Pixel> {
+        for ((x, y), color) in item_pixels {
+            if x >= DISPLAY_SIZE as u32 || y >= DISPLAY_SIZE as u32 {
+                continue;
+            }
+            self.pixels[y as usize][x as usize] = color == 1;
+        }
+    }
+}
+
