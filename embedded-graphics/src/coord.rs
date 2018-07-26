4b2c52845cb89cfa70f479a5c489d19962a61d47
James Waples <jamwaffles@gmail.com>
2018-07-26 14:10:42 +0100

    Add .abs() method to coord

--- embedded-graphics/src/coord.rs
+++ embedded-graphics/src/coord.rs
@@ -1,17 +1,39 @@
 //! 2D coordinate in screen space
 
+type CoordPart = i32;
+
 #[cfg(not(feature = "nalgebra_support"))]
 mod internal_coord {
+    use super::CoordPart;
     use core::ops::{Add, AddAssign, Index, Sub, SubAssign};
 
     /// 2D coordinate type
     #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-    pub struct Coord(pub u32, pub u32);
+    pub struct Coord(pub CoordPart, pub CoordPart);
 
     impl Coord {
         /// Create a new coordinate with X and Y values
-        pub fn new(x: u32, y: u32) -> Self {
+        pub fn new(x: CoordPart, y: CoordPart) -> Self {
             Coord(x, y)
+        }
+
+        /// Clamp coordinate components to positive integer range
+        pub fn clamp_positive(&self) -> Self {
+            Coord(self.0.max(0), self.1.max(0))
+        }
+
+        /// Remove the sign from a coordinate
+        ///
+        ///
+        /// ```
+        /// # use embedded_graphics::coord::Coord;
+        /// #
+        /// let coord = Coord::new(-5, -10);
+        ///
+        /// assert_eq!(coord.abs(), Coord::new(5, 10));
+        /// ```
+        pub fn abs(&self) -> Self {
+            Coord(self.0.abs(), self.1.abs())
         }
     }
 
@@ -46,9 +68,9 @@ mod internal_coord {
     }
 
     impl Index<usize> for Coord {
-        type Output = u32;
+        type Output = CoordPart;
 
-        fn index(&self, idx: usize) -> &u32 {
+        fn index(&self, idx: usize) -> &CoordPart {
             match idx {
                 0 => &self.0,
                 1 => &self.1,
@@ -86,6 +108,14 @@ mod tests {
         let right = Coord::new(10, 20);
 
         assert_eq!(left - right, Coord::new(20, 20));
+    }
+
+    #[test]
+    fn coords_can_be_negative_subtracted() {
+        let left = Coord::new(10, 20);
+        let right = Coord::new(30, 40);
+
+        assert_eq!(left - right, Coord::new(-20, -20));
     }
 
     #[test]
