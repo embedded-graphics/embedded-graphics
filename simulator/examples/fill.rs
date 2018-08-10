7f7d1febe91734df9cd3c1e4268b8a620627be28
James Waples <jamwaffles@gmail.com>
2018-08-10 12:50:07 +0100

    Add shape fill example

---
+++ simulator/examples/fill.rs
@@ -0,0 +1,47 @@
+extern crate embedded_graphics;
+extern crate simulator;
+
+use std::thread;
+use std::time::Duration;
+
+use embedded_graphics::coord::Coord;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::Circle;
+
+use simulator::Display;
+
+fn main() {
+    let mut display = Display::new();
+
+    display.draw(
+        Circle::new(Coord::new(32, 32), 32)
+            .with_stroke(Some(1u8.into()))
+            .into_iter(),
+    );
+
+    display.draw(
+        Circle::new(Coord::new(32, 32), 32)
+            .translate(Coord::new(16, 16))
+            .with_stroke(Some(0u8.into()))
+            .with_fill(Some(1u8.into()))
+            .into_iter(),
+    );
+
+    display.draw(
+        Circle::new(Coord::new(32, 32), 32)
+            .translate(Coord::new(32, 32))
+            .with_stroke(Some(0u8.into()))
+            .with_fill(Some(0u8.into()))
+            .into_iter(),
+    );
+
+    loop {
+        let end = display.run_once();
+
+        if end {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(200));
+    }
+}
