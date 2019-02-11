e943c748553d9e52f3c93905fc090ab6e4555973
James Waples <james@wapl.es>
2019-02-11 19:03:45 +0000

    Initial commit of tinytga

---
+++ tinytga/src/lib.rs
@@ -0,0 +1,12 @@
+//! No-std compatible TGA parser designed for embedded systems, but usable anywhere
+
+#![no_std]
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_copy_implementations)]
+#![deny(trivial_casts)]
+#![deny(trivial_numeric_casts)]
+#![deny(unsafe_code)]
+#![deny(unstable_features)]
+#![deny(unused_import_braces)]
+#![deny(unused_qualifications)]
