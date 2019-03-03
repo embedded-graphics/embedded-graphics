e9a065713d26f5f9f86bd5baaa632e87be83caab
James Waples <james@wapl.es>
2019-03-03 09:53:04 +0000

    Add 16BPP test

---
+++ tinybmp/tests/chessboard-8px-colour-16bit.rs
@@ -0,0 +1,21 @@
+use tinybmp::{Bmp, FileType, Header};
+
+#[test]
+fn chessboard_8px_colour_16bit() {
+    let bmp = Bmp::from_bytes(include_bytes!("./chessboard-8px-colour-16bit.bmp"))
+        .expect("Failed to parse");
+
+    assert_eq!(
+        bmp.header,
+        Header {
+            file_type: FileType::BM,
+            file_size: 266,
+            reserved_1: 0,
+            reserved_2: 0,
+            image_data_start: 138,
+            bpp: 16,
+            image_width: 8,
+            image_height: 8,
+        }
+    );
+}
