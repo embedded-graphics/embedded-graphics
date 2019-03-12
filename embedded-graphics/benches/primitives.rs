eff62329e928474c151f2f084f09e49cf00a686c
James Waples <james@wapl.es>
2019-03-12 20:20:53 +0000

    Add benchmarks for primitives

--- embedded-graphics/benches/primitives.rs
+++ embedded-graphics/benches/primitives.rs
@@ -1,13 +1,47 @@
 use criterion::*;
-use embedded_graphics::{prelude::*, primitives::Circle};
+use embedded_graphics::{
+    pixelcolor::PixelColorU8,
+    prelude::*,
+    primitives::{Circle, Line, Rect},
+};
 
-fn circle(c: &mut Criterion) {
-    c.bench_function("circle", |b| {
-        let circle = Circle::new(Coord::new(10, 10), 10);
+fn filled_circle(c: &mut Criterion) {
+    c.bench_function("filled circle", |b| {
+        let object: Circle<PixelColorU8> = Circle::new(Coord::new(100, 100), 100)
+            .with_fill(Some(1u8.into()))
+            .with_stroke(Some(10u8.into()));
 
-        b.iter(|| circle.into_iter().collect())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<PixelColorU8>>>())
     });
 }
 
-criterion_group!(primitives, circle);
+fn filled_rect(c: &mut Criterion) {
+    c.bench_function("filled rectangle", |b| {
+        let object: Rect<PixelColorU8> = Rect::new(Coord::new(100, 100), Coord::new(200, 200))
+            .with_fill(Some(1u8.into()))
+            .with_stroke(Some(10u8.into()));
+
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<PixelColorU8>>>())
+    });
+}
+
+fn empty_rect(c: &mut Criterion) {
+    c.bench_function("unfilled rectangle", |b| {
+        let object: Rect<PixelColorU8> =
+            Rect::new(Coord::new(100, 100), Coord::new(200, 200)).with_stroke(Some(10u8.into()));
+
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<PixelColorU8>>>())
+    });
+}
+
+fn line(c: &mut Criterion) {
+    c.bench_function("line", |b| {
+        let object: Line<PixelColorU8> =
+            Line::new(Coord::new(100, 100), Coord::new(200, 200)).with_stroke(Some(10u8.into()));
+
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<PixelColorU8>>>())
+    });
+}
+
+criterion_group!(primitives, filled_circle, filled_rect, empty_rect, line);
 criterion_main!(primitives);
