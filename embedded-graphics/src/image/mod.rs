de5d1feeb8cdd41fadee9a1367641efaf7adb972
James Waples <james@wapl.es>
2019-05-05 11:31:11 +0100

    Add doc links for image types

--- embedded-graphics/src/image/mod.rs
+++ embedded-graphics/src/image/mod.rs
@@ -1,4 +1,10 @@
 //! Image object
+//!
+//! If the source image data is in BMP format, use [`ImageBMP`](struct.ImageBmp.html).
+//!
+//! If the source image data is a slice of raw bytes that represents a 1, 8 or 16 bits-per-pixel
+//! (BPP) image, use [`Image1BPP`](type.Image1BPP.html), [`Image8BPP`](type.Image8BPP.html),
+//! or [`Image16BPP`](type.Image16BPP.html) respectively.
 
 mod image;
 mod image16bpp;
