151fa092d8efdbc2d2d2fe68f8cca94bddbaa783
James Waples <james@wapl.es>
2019-06-11 17:05:47 +0100

    Add simulator chaining example

---
+++ simulator/examples/chaining.rs
@@ -0,0 +1,51 @@
+//! Demonstrate the chaining abilities of embedded graphics iterators
+
+extern crate embedded_graphics;
+extern crate simulator;
+
+use std::thread;
+use std::time::Duration;
+
+use embedded_graphics::coord::Coord;
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, Line};
+
+use simulator::{DisplayBuilder, DisplayTheme};
+
+fn main() {
+    let mut display = DisplayBuilder::new().theme(DisplayTheme::OledBlue).build();
+
+    // FIXME: This has to be a separate variable due to usage of `.translate()` returning a new
+    // instance.
+    let text = Font6x8::render_str("Hello World!")
+        .with_stroke(Some(1u8.into()))
+        .translate(Coord::new(5, 50));
+
+    let objects = Circle::new(Coord::new(64, 64), 64)
+        .with_stroke(Some(1u8.into()))
+        .into_iter()
+        .chain(
+            Line::new(Coord::new(64, 64), Coord::new(0, 64))
+                .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        )
+        .chain(
+            Line::new(Coord::new(64, 64), Coord::new(80, 80))
+                .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        )
+        .chain(text.into_iter());
+
+    display.draw(objects);
+
+    loop {
+        let end = display.run_once();
+
+        if end {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(200));
+    }
+}
