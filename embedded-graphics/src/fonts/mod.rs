6a0f98452658cfb421658cb05c3504c6b2c923db
James Waples <james@wapl.es>
2019-06-13 22:15:25 +0100

    Add text drawing macros

--- embedded-graphics/src/fonts/mod.rs
+++ embedded-graphics/src/fonts/mod.rs
@@ -54,3 +54,124 @@ where
     #[deprecated(since = "0.4.5", note = "use `.size()` instead")]
     fn dimensions(&self) -> UnsignedCoord;
 }
+
+/// Internal macro used to implement `text_*` on fonts. Do not use directly!
+#[doc(hidden)]
+#[macro_export]
+macro_rules! impl_text {
+    ($Font:ident, $text:expr $(, $style_key:ident = $style_value:expr )* $(,)?) => {{
+        #[allow(unused_imports)]
+        use $crate::style::WithStyle;
+        $crate::fonts::$Font::render_str($text)
+            $( .$style_key($style_value) )*
+    }};
+}
+
+/// Render text using the [`Font6x8`](./fonts/type.Font6x8.html) font
+///
+/// ```rust
+/// use embedded_graphics::{text_6x8, prelude::*, fonts::Font6x8};
+///
+/// let text: Font6x8<u8> = text_6x8!("Hello world!");
+/// let styled_text: Font6x8<u8> = text_6x8!(
+///     "Hello world!",
+///     stroke = Some(10u8),
+///     fill = Some(20u8)
+/// );
+/// ```
+///
+/// Style properties like `stroke` map to the method calls on the
+/// [`WithStyle`](./style/trait.WithStyle.html) trait.
+#[macro_export]
+macro_rules! text_6x8 {
+    ($text:expr $(, $style_key:ident = $style_value:expr )* $(,)?) => {
+        $crate::impl_text!(Font6x8, $text $(, $style_key = $style_value )*)
+    };
+}
+
+/// Render text using the [`Font6x12`](./fonts/type.Font6x12.html) font
+///
+/// ```rust
+/// use embedded_graphics::{text_6x12, prelude::*, fonts::Font6x12};
+///
+/// let text: Font6x12<u8> = text_6x12!("Hello world!");
+/// let styled_text: Font6x12<u8> = text_6x12!(
+///     "Hello world!",
+///     stroke = Some(10u8),
+///     fill = Some(20u8)
+/// );
+/// ```
+///
+/// Style properties like `stroke` map to the method calls on the
+/// [`WithStyle`](./style/trait.WithStyle.html) trait.
+#[macro_export]
+macro_rules! text_6x12 {
+    ($text:expr $(, $style_key:ident = $style_value:expr )* $(,)?) => {
+        $crate::impl_text!(Font6x12, $text $(, $style_key = $style_value )*)
+    };
+}
+
+/// Render text using the [`Font8x16`](./fonts/type.Font8x16.html) font
+///
+/// ```rust
+/// use embedded_graphics::{text_8x16, prelude::*, fonts::Font8x16};
+///
+/// let text: Font8x16<u8> = text_8x16!("Hello world!");
+/// let styled_text: Font8x16<u8> = text_8x16!(
+///     "Hello world!",
+///     stroke = Some(10u8),
+///     fill = Some(20u8)
+/// );
+/// ```
+///
+/// Style properties like `stroke` map to the method calls on the
+/// [`WithStyle`](./style/trait.WithStyle.html) trait.
+#[macro_export]
+macro_rules! text_8x16 {
+    ($text:expr $(, $style_key:ident = $style_value:expr )* $(,)?) => {
+        $crate::impl_text!(Font8x16, $text $(, $style_key = $style_value )*)
+    };
+}
+
+/// Render text using the [`Font12x16`](./fonts/type.Font12x16.html) font
+///
+/// ```rust
+/// use embedded_graphics::{text_12x16, prelude::*, fonts::Font12x16};
+///
+/// let text: Font12x16<u8> = text_12x16!("Hello world!");
+/// let styled_text: Font12x16<u8> = text_12x16!(
+///     "Hello world!",
+///     stroke = Some(10u8),
+///     fill = Some(20u8)
+/// );
+/// ```
+///
+/// Style properties like `stroke` map to the method calls on the
+/// [`WithStyle`](./style/trait.WithStyle.html) trait.
+#[macro_export]
+macro_rules! text_12x16 {
+    ($text:expr $(, $style_key:ident = $style_value:expr )* $(,)?) => {
+        $crate::impl_text!(Font12x16, $text $(, $style_key = $style_value )*)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn font_macros() {
+        let _text: Font6x8<u8> = text_6x8!("Hello!");
+        let _text: Font6x12<u8> = text_6x12!("Hello!");
+        let _text: Font8x16<u8> = text_8x16!("Hello!");
+        let _text: Font12x16<u8> = text_12x16!("Hello!");
+    }
+
+    #[test]
+    fn styled_text() {
+        let _text: Font6x8<u8> = text_6x8!("Hello!", stroke = Some(10u8));
+        let _text: Font6x12<u8> = text_6x12!("Hello!", stroke = Some(10u8));
+        let _text: Font8x16<u8> = text_8x16!("Hello!", stroke = Some(10u8));
+        let _text: Font12x16<u8> = text_12x16!("Hello!", stroke = Some(10u8));
+    }
+}
