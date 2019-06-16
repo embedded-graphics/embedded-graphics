ae1f84ccc350e43dd70d459729c8c4a4694dec64
James Waples <james@wapl.es>
2019-06-16 12:37:16 +0100

    Move assets folder to root

--- embedded-graphics/src/lib.rs
+++ embedded-graphics/src/lib.rs
@@ -22,7 +22,7 @@
 //!
 //! Embedded graphics comes with a [simulator]!
 //!
-//! ![Example screenshots of the simulator](https://raw.githubusercontent.com/jamwaffles/embedded-graphics/master/embedded-graphics/assets/simulator-demo.png)
+//! ![Example screenshots of the simulator](https://raw.githubusercontent.com/jamwaffles/embedded-graphics/master/assets/simulator-demo.png)
 //!
 //! Take a look at the [simulator examples] to see what
 //! embedded_graphics can do, and how it might look on a display. You can run the examples like
