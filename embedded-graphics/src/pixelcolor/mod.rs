2ad954f38006fb2e0933ca3ce178dba722f6b75f
Ralf Fuest <mail@rfuest.de>
2019-06-26 20:46:15 +0200

    Add missing color conversions

--- embedded-graphics/src/pixelcolor/mod.rs
+++ embedded-graphics/src/pixelcolor/mod.rs
@@ -1,6 +1,6 @@
 //! Pixel color
 
-use byteorder::{ByteOrder, BigEndian, LittleEndian};
+use byteorder::{BigEndian, ByteOrder, LittleEndian};
 use core::fmt;
 
 /// Pixel color trait
@@ -35,10 +35,10 @@ pub trait RgbColor {
     /// maximum value in red channel
     const MAX_R: u8;
 
-    /// maximum value in red channel
+    /// maximum value in green channel
     const MAX_G: u8;
 
-    /// maximum value in red channel
+    /// maximum value in blue channel
     const MAX_B: u8;
 
     /// black
@@ -66,84 +66,118 @@ pub trait RgbColor {
     const WHITE: Self;
 }
 
-macro_rules! rgb_color {
-    ($name: ident, $type: ident, $r_mask: expr, $g_mask: expr, $b_mask: expr, $r_pos: expr, $g_pos: expr, $b_pos: expr) => {
+macro_rules! impl_rgb_color {
+    ($type: ident, $base_type: ident, ($r_bits: expr, $g_bits: expr, $b_bits: expr), ($r_pos: expr, $g_pos: expr, $b_pos: expr)) => {
         /// Color
         #[derive(Clone, Copy, PartialEq)]
-        pub struct $name($type);
+        pub struct $type($base_type);
 
-        impl fmt::Debug for $name {
+        impl fmt::Debug for $type {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
                 write!(
                     f,
                     "{}(r: {}, g: {}, b: {})",
-                    stringify!($name),
+                    stringify!($type),
                     self.r(),
                     self.g(),
                     self.b()
                 )
             }
         }
-        impl $name {
+
+        impl $type
+        where
+            Self: RgbColor,
+        {
             /// New
             pub const fn new(r: u8, g: u8, b: u8) -> Self {
                 #![allow(trivial_numeric_casts)]
 
                 Self(
-                    ((r as $type & $r_mask) << $r_pos)
-                        | ((g as $type & $g_mask) << $g_pos)
-                        | ((b as $type & $b_mask) << $b_pos),
+                    ((r & Self::MAX_R) as $base_type) << $r_pos
+                        | ((g & Self::MAX_G) as $base_type) << $g_pos
+                        | ((b & Self::MAX_B) as $base_type) << $b_pos,
                 )
             }
         }
 
-        impl PixelColor for $name {
+        impl PixelColor for $type {
             const DEFAULT_BG: Self = Self::BLACK;
             const DEFAULT_FG: Self = Self::WHITE;
         }
 
-        impl RgbColor for $name {
+        impl RgbColor for $type {
             fn r(&self) -> u8 {
                 #![allow(trivial_numeric_casts)]
 
-                ((self.0 >> $r_pos) & $r_mask) as u8
+                (self.0 >> $r_pos) as u8 & Self::MAX_R
             }
 
             fn g(&self) -> u8 {
                 #![allow(trivial_numeric_casts)]
 
-                ((self.0 >> $g_pos) & $g_mask) as u8
+                (self.0 >> $g_pos) as u8 & Self::MAX_G
             }
 
             fn b(&self) -> u8 {
                 #![allow(trivial_numeric_casts)]
 
-                ((self.0 >> $b_pos) & $b_mask) as u8
+                (self.0 >> $b_pos) as u8 & Self::MAX_B
             }
 
-            const MAX_R: u8 = $r_mask;
-            const MAX_G: u8 = $g_mask;
-            const MAX_B: u8 = $b_mask;
+            const MAX_R: u8 = ((1usize << $r_bits) - 1) as u8;
+            const MAX_G: u8 = ((1usize << $g_bits) - 1) as u8;
+            const MAX_B: u8 = ((1usize << $b_bits) - 1) as u8;
 
             const BLACK: Self = Self::new(0, 0, 0);
-            const RED: Self = Self::new($r_mask, 0, 0);
-            const GREEN: Self = Self::new(0, $g_mask, 0);
-            const BLUE: Self = Self::new(0, 0, $b_mask);
-            const YELLOW: Self = Self::new($r_mask, $g_mask, 0);
-            const MAGENTA: Self = Self::new($r_mask, 0, $b_mask);
-            const CYAN: Self = Self::new(0, $g_mask, $b_mask);
-            const WHITE: Self = Self::new($r_mask, $g_mask, $b_mask);
+            const RED: Self = Self::new(Self::MAX_R, 0, 0);
+            const GREEN: Self = Self::new(0, Self::MAX_G, 0);
+            const BLUE: Self = Self::new(0, 0, Self::MAX_B);
+            const YELLOW: Self = Self::new(Self::MAX_R, Self::MAX_G, 0);
+            const MAGENTA: Self = Self::new(Self::MAX_R, 0, Self::MAX_B);
+            const CYAN: Self = Self::new(0, Self::MAX_G, Self::MAX_B);
+            const WHITE: Self = Self::new(Self::MAX_R, Self::MAX_G, Self::MAX_B);
         }
 
-        impl From<$name> for $type {
-            fn from(color: $name) -> Self {
+        impl From<$type> for $base_type {
+            fn from(color: $type) -> Self {
                 color.0
             }
         }
     };
 }
 
-macro_rules! from_slice_u16 {
+macro_rules! impl_rgb_bgr_pair {
+    (($rgb_type: ident, $bgr_type: ident) : $base_type: ident, ($r_bits: expr, $g_bits: expr, $b_bits: expr)) => {
+        impl_rgb_color!(
+            $rgb_type,
+            $base_type,
+            ($r_bits, $g_bits, $b_bits),
+            ($b_bits + $g_bits, $b_bits, 0)
+        );
+
+        impl_rgb_color!(
+            $bgr_type,
+            $base_type,
+            ($r_bits, $g_bits, $b_bits),
+            (0, $r_bits, $r_bits + $g_bits)
+        );
+
+        impl From<$rgb_type> for $bgr_type {
+            fn from(c: $rgb_type) -> Self {
+                Self::new(c.r(), c.g(), c.b())
+            }
+        }
+
+        impl From<$bgr_type> for $rgb_type {
+            fn from(c: $bgr_type) -> Self {
+                Self::new(c.r(), c.g(), c.b())
+            }
+        }
+    };
+}
+
+macro_rules! impl_from_slice_u16 {
     ($type: ident) => {
         impl FromSlice for $type {
             fn from_le_slice(data: &[u8]) -> Self {
@@ -157,7 +191,7 @@ macro_rules! from_slice_u16 {
     };
 }
 
-macro_rules! from_slice_u32 {
+macro_rules! impl_from_slice_u32 {
     ($type: ident) => {
         impl FromSlice for $type {
             fn from_le_slice(data: &[u8]) -> Self {
@@ -179,68 +213,57 @@ macro_rules! from_slice_u32 {
     };
 }
 
-/// Rgb555
-rgb_color!(Rgb555, u16, 0x1F, 0x1F, 0x1F, 10, 5, 0);
-from_slice_u16!(Rgb555);
+impl_rgb_bgr_pair!((Rgb555, Bgr555): u16, (5, 5, 5));
+impl_rgb_bgr_pair!((Rgb565, Bgr565): u16, (5, 6, 5));
+impl_rgb_bgr_pair!((Rgb888, Bgr888): u32, (8, 8, 8));
 
-/// Bgr555
-rgb_color!(Bgr555, u16, 0x1F, 0x1F, 0x1F, 10, 5, 0);
-from_slice_u16!(Bgr555);
+impl_from_slice_u16!(Rgb555);
+impl_from_slice_u16!(Bgr555);
+impl_from_slice_u16!(Rgb565);
+impl_from_slice_u16!(Bgr565);
 
-/// Rgb565
-rgb_color!(Rgb565, u16, 0x1F, 0x3F, 0x1F, 11, 5, 0);
-from_slice_u16!(Rgb565);
+impl_from_slice_u32!(Rgb888);
+impl_from_slice_u32!(Bgr888);
 
-/// Bgr565
-rgb_color!(Bgr565, u16, 0x1F, 0x3F, 0x1F, 0, 5, 11);
-from_slice_u16!(Bgr565);
-
-/// Rgb888
-rgb_color!(Rgb888, u32, 0xFF, 0xFF, 0xFF, 16, 8, 0);
-from_slice_u32!(Rgb888);
-
-/// Bgr888
-rgb_color!(Bgr888, u32, 0xFF, 0xFF, 0xFF, 0, 8, 16);
-from_slice_u32!(Bgr888);
-
-/// Binary color
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum BinaryColor {
-    /// inactive pixel
-    Off,
-
-    /// active pixel
-    On,
+macro_rules! convert_channel {
+    ($value: expr, $from_max: expr, $to_max: expr) => {
+        (($value as u16 * $to_max as u16 + $from_max as u16 / 2) / $from_max as u16) as u8
+    };
 }
 
-impl PixelColor for BinaryColor {
-    const DEFAULT_BG: BinaryColor = BinaryColor::Off;
-    const DEFAULT_FG: BinaryColor = BinaryColor::On;
+macro_rules! impl_rgb_conversion {
+    ($type: ident, ($($other_type: ident),+)) => {
+        $(
+            impl From<$other_type> for $type {
+                fn from(other: $other_type) -> Self {
+                    Self::new(
+                        convert_channel!(other.r(), $other_type::MAX_R, $type::MAX_R),
+                        convert_channel!(other.g(), $other_type::MAX_G, $type::MAX_G),
+                        convert_channel!(other.b(), $other_type::MAX_B, $type::MAX_B),
+                    )
+                }
+            }
+        )*
+
+        impl From<Y8> for $type {
+            fn from(other: Y8) -> Self {
+                Self::new(
+                    convert_channel!(other.y(), Y8::MAX_Y, $type::MAX_R),
+                    convert_channel!(other.y(), Y8::MAX_Y, $type::MAX_G),
+                    convert_channel!(other.y(), Y8::MAX_Y, $type::MAX_B),
+                )
+
+            }
+        }
+    };
 }
 
-impl From<Rgb565> for Rgb888 {
-    fn from(color: Rgb565) -> Self {
-        //TODO: add rounding
-        let r = color.r() as u16 * 255 / 31;
-        let g = color.g() as u16 * 255 / 63;
-        let b = color.b() as u16 * 255 / 31;
-
-        Self::new(r as u8, g as u8, b as u8)
-    }
-}
-
-impl From<Rgb888> for Rgb565 {
-    fn from(color: Rgb888) -> Self {
-        //TODO: add rounding
-        let r = color.r() >> 3;
-        let g = color.g() >> 2;
-        let b = color.b() >> 3;
-
-        Self::new(r, g, b)
-    }
-}
-
-//TODO: add more conversions
+impl_rgb_conversion!(Rgb555, (Rgb565, Bgr565, Rgb888, Bgr888));
+impl_rgb_conversion!(Bgr555, (Rgb565, Bgr565, Rgb888, Bgr888));
+impl_rgb_conversion!(Rgb565, (Rgb555, Bgr555, Rgb888, Bgr888));
+impl_rgb_conversion!(Bgr565, (Rgb555, Bgr555, Rgb888, Bgr888));
+impl_rgb_conversion!(Rgb888, (Rgb555, Bgr555, Rgb565, Bgr565));
+impl_rgb_conversion!(Bgr888, (Rgb555, Bgr555, Rgb565, Bgr565));
 
 /// 8bit luma
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -256,6 +279,9 @@ impl Y8 {
     pub fn y(&self) -> u8 {
         self.0
     }
+
+    /// maximum value in luminance channel
+    pub const MAX_Y: u8 = 255;
 
     /// black
     pub const BLACK: Y8 = Self::new(0);
@@ -279,27 +305,69 @@ impl FromSlice for Y8 {
     }
 }
 
+/// Binary color
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BinaryColor {
+    /// inactive pixel
+    Off,
+
+    /// active pixel
+    On,
+}
+
+impl BinaryColor {
+    /// invert color
+    pub fn invert(&self) -> Self {
+        match self {
+            BinaryColor::On => BinaryColor::Off,
+            BinaryColor::Off => BinaryColor::On,
+        }
+    }
+
+    /// map colors
+    pub fn map_color<T: PixelColor>(&self, color_off: T, color_on: T) -> T {
+        match self {
+            BinaryColor::On => color_on,
+            BinaryColor::Off => color_off,
+        }
+    }
+}
+
+impl PixelColor for BinaryColor {
+    const DEFAULT_BG: BinaryColor = BinaryColor::Off;
+    const DEFAULT_FG: BinaryColor = BinaryColor::On;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn new() {
-        let c = Rgb565::new(1, 2, 3);
-        assert_eq!(c.r(), 1);
-        assert_eq!(c.g(), 2);
-        assert_eq!(c.b(), 3);
+    macro_rules! test_rgb_conversions {
+        ($type: ident, ($($other_type: ident),+)) => {
+            $(
+                assert_eq!($type::from($other_type::BLACK), $type::BLACK);
+                assert_eq!($type::from($other_type::RED), $type::RED);
+                assert_eq!($type::from($other_type::GREEN), $type::GREEN);
+                assert_eq!($type::from($other_type::BLUE), $type::BLUE);
+                assert_eq!($type::from($other_type::YELLOW), $type::YELLOW);
+                assert_eq!($type::from($other_type::MAGENTA), $type::MAGENTA);
+                assert_eq!($type::from($other_type::CYAN), $type::CYAN);
+                assert_eq!($type::from($other_type::WHITE), $type::WHITE);
+            )*
 
-        let c = Bgr565::new(1, 2, 3);
-        assert_eq!(c.r(), 1);
-        assert_eq!(c.g(), 2);
-        assert_eq!(c.b(), 3);
+            assert_eq!($type::from(Y8::BLACK), $type::BLACK);
+            assert_eq!($type::from(Y8::WHITE), $type::WHITE);
+        }
     }
 
     #[test]
-    fn convert_rgb565_to_rgb888() {
-        assert_eq!(Rgb888::from(Rgb565::BLACK), Rgb888::BLACK);
-        assert_eq!(Rgb888::from(Rgb565::WHITE), Rgb888::WHITE);
+    fn rgb_color_constant_conversions() {
+        test_rgb_conversions!(Rgb555, (Rgb555, Bgr555, Rgb565, Bgr565, Rgb888, Bgr888));
+        test_rgb_conversions!(Bgr555, (Rgb555, Rgb555, Rgb565, Bgr565, Rgb888, Bgr888));
+        test_rgb_conversions!(Rgb565, (Rgb555, Bgr555, Rgb565, Bgr565, Rgb888, Bgr888));
+        test_rgb_conversions!(Bgr565, (Rgb555, Bgr555, Rgb565, Bgr565, Rgb888, Bgr888));
+        test_rgb_conversions!(Rgb888, (Rgb555, Bgr555, Rgb565, Bgr565, Rgb888, Bgr888));
+        test_rgb_conversions!(Bgr888, (Rgb555, Bgr555, Rgb565, Bgr565, Rgb888, Bgr888));
     }
 
     #[test]
@@ -329,5 +397,21 @@ mod tests {
         }
     }
 
-    //TODO: add additional tests
+    #[test]
+    fn invert_binary_color() {
+        assert_eq!(BinaryColor::Off.invert(), BinaryColor::On);
+        assert_eq!(BinaryColor::On.invert(), BinaryColor::Off);
+    }
+
+    #[test]
+    fn map_binary_color() {
+        assert_eq!(
+            BinaryColor::Off.map_color(Rgb565::RED, Rgb565::GREEN),
+            Rgb565::RED
+        );
+        assert_eq!(
+            BinaryColor::On.map_color(Rgb565::RED, Rgb565::GREEN),
+            Rgb565::GREEN
+        );
+    }
 }
