21bc66b7762dabcd29fa8e922d41525d18d83668
Ralf Fuest <mail@rfuest.de>
2019-06-28 17:18:49 +0200

    Fix benches

--- embedded-graphics/benches/fonts.rs
+++ embedded-graphics/benches/fonts.rs
@@ -1,22 +1,23 @@
 use criterion::*;
 use embedded_graphics::{
     fonts::{Font12x16, Font6x8},
+    pixelcolor::Y8,
     prelude::*,
 };
 
 fn font_6x8(c: &mut Criterion) {
     c.bench_function("font 6x8 Hello world!", |b| {
-        let object: Font6x8<u8> = Font6x8::render_str("Hello world!").stroke(Some(10));
+        let object: Font6x8<Y8> = Font6x8::render_str("Hello world!").stroke(Some(Y8::new(10)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
 fn font_12x16(c: &mut Criterion) {
     c.bench_function("font 12x16 Hello world!", |b| {
-        let object: Font12x16<u8> = Font12x16::render_str("Hello world!").stroke(Some(10u8.into()));
+        let object: Font12x16<Y8> = Font12x16::render_str("Hello world!").stroke(Some(Y8::new(10)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
--- embedded-graphics/benches/primitives.rs
+++ embedded-graphics/benches/primitives.rs
@@ -1,63 +1,64 @@
 use criterion::*;
 use embedded_graphics::{
+    pixelcolor::Y8,
     prelude::*,
     primitives::{Circle, Line, Rectangle, Triangle},
 };
 
 fn filled_circle(c: &mut Criterion) {
     c.bench_function("filled circle", |b| {
-        let object: Circle<u8> = Circle::new(Coord::new(100, 100), 100)
-            .fill(Some(1u8.into()))
-            .stroke(Some(10u8.into()));
+        let object: Circle<Y8> = Circle::new(Coord::new(100, 100), 100)
+            .fill(Some(Y8::new(1)))
+            .stroke(Some(Y8::new(10)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
 fn filled_rect(c: &mut Criterion) {
     c.bench_function("filled rectangle", |b| {
-        let object: Rectangle<u8> = Rectangle::new(Coord::new(100, 100), Coord::new(200, 200))
-            .fill(Some(1u8.into()))
-            .stroke(Some(10u8.into()));
+        let object: Rectangle<Y8> = Rectangle::new(Coord::new(100, 100), Coord::new(200, 200))
+            .fill(Some(Y8::new(1)))
+            .stroke(Some(Y8::new(10)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
 fn empty_rect(c: &mut Criterion) {
     c.bench_function("unfilled rectangle", |b| {
-        let object: Rectangle<u8> =
-            Rectangle::new(Coord::new(100, 100), Coord::new(200, 200)).stroke(Some(10u8.into()));
+        let object: Rectangle<Y8> =
+            Rectangle::new(Coord::new(100, 100), Coord::new(200, 200)).stroke(Some(Y8::new(10)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
 fn line(c: &mut Criterion) {
     c.bench_function("line", |b| {
-        let object: Line<u8> =
-            Line::new(Coord::new(100, 100), Coord::new(200, 200)).stroke(Some(10u8.into()));
+        let object: Line<Y8> =
+            Line::new(Coord::new(100, 100), Coord::new(200, 200)).stroke(Some(Y8::new(10)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
 fn triangle(c: &mut Criterion) {
     c.bench_function("triangle", |b| {
-        let object: Triangle<u8> =
+        let object: Triangle<Y8> =
             Triangle::new(Coord::new(5, 10), Coord::new(15, 20), Coord::new(5, 20));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
 fn filled_triangle(c: &mut Criterion) {
     c.bench_function("filled_triangle", |b| {
-        let object: Triangle<u8> =
+        let object: Triangle<Y8> =
             Triangle::new(Coord::new(5, 10), Coord::new(15, 20), Coord::new(5, 20))
-                .fill(Some(1u8.into()));
+                .fill(Some(Y8::new(1)));
 
-        b.iter(|| object.into_iter().collect::<Vec<Pixel<u8>>>())
+        b.iter(|| object.into_iter().collect::<Vec<Pixel<Y8>>>())
     });
 }
 
