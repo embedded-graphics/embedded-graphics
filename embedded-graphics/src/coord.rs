5e04261189f059b638a816688b39799f438a5640
James Waples <jamwaffles@gmail.com>
2019-08-19 21:17:39 +0100

    Remove Nalgebra alias, impl From<Vector2>

--- embedded-graphics/src/coord.rs
+++ embedded-graphics/src/coord.rs
@@ -2,184 +2,192 @@
 //!
 //! To output (non-negative) pixel coordinates, use [`UnsignedCoord`](../unsignedcoord/index.html).
 
-use super::unsignedcoord::UnsignedCoord;
+use crate::unsignedcoord::UnsignedCoord;
+use core::ops::{Add, AddAssign, Index, Neg, Sub, SubAssign};
 
 type CoordPart = i32;
 
-#[cfg(not(feature = "nalgebra_support"))]
-mod internal_coord {
-    use super::*;
-    use core::ops::{Add, AddAssign, Index, Neg, Sub, SubAssign};
+/// 2D signed integer coordinate type
+///
+/// This coordinate should be used to define graphics object coordinates. For example, a
+/// [`Rect`] may be defined that has its top left at `(-1,-2)`. To specify positive-only screen
+/// coordinates and the like, see [`UnsignedCoord`].
+///
+/// ```rust
+/// use embedded_graphics::{coord::Coord, icoord};
+///
+/// // Create a coord using the `new` constructor method
+/// let c1 = Coord::new(10, 20);
+///
+/// // Create a coord using the handy `icoord` macro
+/// let c2 = icoord!(10, 20);
+///
+/// assert_eq!(c1, c2);
+/// ```
+///
+/// Note that enabling the `nalgebra` feature will alias Nalgebra's [`Vector2<i32>`] type to
+/// `Coord` instead of this builtin implementation.
+///
+/// [`UnsignedCoord`]: ../unsignedcoord/struct.UnsignedCoord.html
+/// [`Rect`]: ../primitives/rectangle/struct.Rectangle.html
+/// [`Vector2<i32>`]: https://docs.rs/nalgebra/0.18.0/nalgebra/base/type.Vector2.html
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct Coord(pub CoordPart, pub CoordPart);
 
-    /// 2D signed integer coordinate type
+impl Coord {
+    /// Create a new coordinate with X and Y values
+    pub fn new(x: CoordPart, y: CoordPart) -> Self {
+        Coord(x, y)
+    }
+
+    /// Clamp coordinate components to positive integer range
     ///
-    /// This coordinate should be used to define graphics object coordinates. For example, a
-    /// [`Rect`] may be defined that has its top left at `(-1,-2)`. To specify positive-only screen
-    /// coordinates and the like, see [`UnsignedCoord`].
-    ///
-    /// ```rust
-    /// use embedded_graphics::{coord::Coord, icoord};
-    ///
-    /// // Create a coord using the `new` constructor method
-    /// let c1 = Coord::new(10, 20);
-    ///
-    /// // Create a coord using the handy `icoord` macro
-    /// let c2 = icoord!(10, 20);
-    ///
-    /// assert_eq!(c1, c2);
     /// ```
+    /// # use embedded_graphics::coord::Coord;
+    /// #
+    /// let coord = Coord::new(-5, 10);
     ///
-    /// Note that enabling the `nalgebra` feature will alias Nalgebra's [`Vector2<i32>`] type to
-    /// `Coord` instead of this builtin implementation.
+    /// assert_eq!(coord.clamp_positive(), Coord::new(0, 10));
+    /// ```
+    pub fn clamp_positive(&self) -> Self {
+        Coord::new(self.0.max(0), self.1.max(0))
+    }
+
+    /// Remove the sign from a coordinate
     ///
-    /// [`UnsignedCoord`]: ../unsignedcoord/struct.UnsignedCoord.html
-    /// [`Rect`]: ../primitives/rectangle/struct.Rectangle.html
-    /// [`Vector2<i32>`]: https://docs.rs/nalgebra/0.18.0/nalgebra/base/type.Vector2.html
-    #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-    pub struct Coord(pub CoordPart, pub CoordPart);
-
-    impl Coord {
-        /// Create a new coordinate with X and Y values
-        pub fn new(x: CoordPart, y: CoordPart) -> Self {
-            Coord(x, y)
-        }
-
-        /// Clamp coordinate components to positive integer range
-        ///
-        /// ```
-        /// # use embedded_graphics::coord::Coord;
-        /// #
-        /// let coord = Coord::new(-5, 10);
-        ///
-        /// assert_eq!(coord.clamp_positive(), Coord::new(0, 10));
-        /// ```
-        pub fn clamp_positive(&self) -> Self {
-            Coord::new(self.0.max(0), self.1.max(0))
-        }
-
-        /// Remove the sign from a coordinate
-        ///
-        ///
-        /// ```
-        /// # use embedded_graphics::coord::Coord;
-        /// #
-        /// let coord = Coord::new(-5, -10);
-        ///
-        /// assert_eq!(coord.abs(), Coord::new(5, 10));
-        /// ```
-        pub fn abs(&self) -> Self {
-            Coord::new(self.0.abs(), self.1.abs())
-        }
+    ///
+    /// ```
+    /// # use embedded_graphics::coord::Coord;
+    /// #
+    /// let coord = Coord::new(-5, -10);
+    ///
+    /// assert_eq!(coord.abs(), Coord::new(5, 10));
+    /// ```
+    pub fn abs(&self) -> Self {
+        Coord::new(self.0.abs(), self.1.abs())
     }
+}
 
-    impl Add for Coord {
-        type Output = Coord;
+impl Add for Coord {
+    type Output = Coord;
 
-        fn add(self, other: Coord) -> Coord {
-            Coord::new(self.0 + other.0, self.1 + other.1)
-        }
+    fn add(self, other: Coord) -> Coord {
+        Coord::new(self.0 + other.0, self.1 + other.1)
     }
+}
 
-    impl AddAssign for Coord {
-        fn add_assign(&mut self, other: Coord) {
-            self.0 += other.0;
-            self.1 += other.1;
-        }
+impl AddAssign for Coord {
+    fn add_assign(&mut self, other: Coord) {
+        self.0 += other.0;
+        self.1 += other.1;
     }
+}
 
-    impl Sub for Coord {
-        type Output = Coord;
+impl Sub for Coord {
+    type Output = Coord;
 
-        fn sub(self, other: Coord) -> Coord {
-            Coord::new(self.0 - other.0, self.1 - other.1)
-        }
+    fn sub(self, other: Coord) -> Coord {
+        Coord::new(self.0 - other.0, self.1 - other.1)
     }
+}
 
-    impl SubAssign for Coord {
-        fn sub_assign(&mut self, other: Coord) {
-            self.0 -= other.0;
-            self.1 -= other.1;
-        }
+impl SubAssign for Coord {
+    fn sub_assign(&mut self, other: Coord) {
+        self.0 -= other.0;
+        self.1 -= other.1;
     }
+}
 
-    impl Index<usize> for Coord {
-        type Output = CoordPart;
+impl Index<usize> for Coord {
+    type Output = CoordPart;
 
-        fn index(&self, idx: usize) -> &CoordPart {
-            match idx {
-                0 => &self.0,
-                1 => &self.1,
-                _ => panic!("Unreachable index {}", idx),
-            }
-        }
-    }
-
-    impl Neg for Coord {
-        type Output = Coord;
-
-        fn neg(self) -> Self::Output {
-            Coord::new(-self.0, -self.1)
-        }
-    }
-
-    impl From<(u32, u32)> for Coord {
-        fn from(other: (u32, u32)) -> Self {
-            Self(other.0 as i32, other.1 as i32)
-        }
-    }
-
-    impl From<[u32; 2]> for Coord {
-        fn from(other: [u32; 2]) -> Self {
-            Self(other[0] as i32, other[1] as i32)
-        }
-    }
-
-    impl From<&[u32; 2]> for Coord {
-        fn from(other: &[u32; 2]) -> Self {
-            Self(other[0] as i32, other[1] as i32)
-        }
-    }
-
-    impl From<(i32, i32)> for Coord {
-        fn from(other: (i32, i32)) -> Self {
-            Self(other.0, other.1)
-        }
-    }
-
-    impl From<[i32; 2]> for Coord {
-        fn from(other: [i32; 2]) -> Self {
-            Self(other[0], other[1])
-        }
-    }
-
-    impl From<&[i32; 2]> for Coord {
-        fn from(other: &[i32; 2]) -> Self {
-            Self(other[0], other[1])
-        }
-    }
-
-    impl From<Coord> for (i32, i32) {
-        fn from(other: Coord) -> (i32, i32) {
-            (other.0, other.1)
-        }
-    }
-
-    impl From<&Coord> for (i32, i32) {
-        fn from(other: &Coord) -> (i32, i32) {
-            (other.0, other.1)
+    fn index(&self, idx: usize) -> &CoordPart {
+        match idx {
+            0 => &self.0,
+            1 => &self.1,
+            _ => panic!("Unreachable index {}", idx),
         }
     }
 }
 
-#[cfg(not(feature = "nalgebra_support"))]
-pub use self::internal_coord::Coord;
+impl Neg for Coord {
+    type Output = Coord;
+
+    fn neg(self) -> Self::Output {
+        Coord::new(-self.0, -self.1)
+    }
+}
+
+impl From<(u32, u32)> for Coord {
+    fn from(other: (u32, u32)) -> Self {
+        Self(other.0 as i32, other.1 as i32)
+    }
+}
+
+impl From<[u32; 2]> for Coord {
+    fn from(other: [u32; 2]) -> Self {
+        Self(other[0] as i32, other[1] as i32)
+    }
+}
+
+impl From<&[u32; 2]> for Coord {
+    fn from(other: &[u32; 2]) -> Self {
+        Self(other[0] as i32, other[1] as i32)
+    }
+}
+
+impl From<(i32, i32)> for Coord {
+    fn from(other: (i32, i32)) -> Self {
+        Self(other.0, other.1)
+    }
+}
+
+impl From<[i32; 2]> for Coord {
+    fn from(other: [i32; 2]) -> Self {
+        Self(other[0], other[1])
+    }
+}
+
+impl From<&[i32; 2]> for Coord {
+    fn from(other: &[i32; 2]) -> Self {
+        Self(other[0], other[1])
+    }
+}
+
+impl From<Coord> for (i32, i32) {
+    fn from(other: Coord) -> (i32, i32) {
+        (other.0, other.1)
+    }
+}
+
+impl From<&Coord> for (i32, i32) {
+    fn from(other: &Coord) -> (i32, i32) {
+        (other.0, other.1)
+    }
+}
 
 #[cfg(feature = "nalgebra_support")]
-use nalgebra;
+use nalgebra::{base::Scalar, Vector2};
 
-/// 2D coordinate type with Nalgebra support
 #[cfg(feature = "nalgebra_support")]
-pub type Coord = nalgebra::Vector2<CoordPart>;
+impl<N> From<Vector2<N>> for Coord
+where
+    N: Into<CoordPart> + Scalar,
+{
+    fn from(other: Vector2<N>) -> Self {
+        Self::new(other[0].into(), other[1].into())
+    }
+}
+
+#[cfg(feature = "nalgebra_support")]
+impl<N> From<&Vector2<N>> for Coord
+where
+    N: Into<CoordPart> + Scalar,
+{
+    fn from(other: &Vector2<N>) -> Self {
+        Self::new(other[0].into(), other[1].into())
+    }
+}
 
 /// Convert a value to an unsigned coordinate
 pub trait ToUnsigned {
@@ -248,27 +256,18 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(feature = "nalgebra_support"))]
     fn from_tuple() {
         assert_eq!(Coord::from((20u32, 30u32)), Coord::new(20, 30));
         assert_eq!(Coord::from((20i32, 30i32)), Coord::new(20, 30));
     }
 
     #[test]
-    #[cfg(not(feature = "nalgebra_support"))]
     fn from_array() {
         assert_eq!(Coord::from([20u32, 30u32]), Coord::new(20, 30));
         assert_eq!(Coord::from([20i32, 30i32]), Coord::new(20, 30));
     }
 
     #[test]
-    #[cfg(feature = "nalgebra_support")]
-    fn from_array() {
-        assert_eq!(Coord::from([20i32, 30i32]), Coord::new(20, 30));
-    }
-
-    #[test]
-    #[cfg(not(feature = "nalgebra_support"))]
     fn from_array_ref() {
         assert_eq!(Coord::from(&[20u32, 30u32]), Coord::new(20, 30));
         assert_eq!(Coord::from(&[20i32, 30i32]), Coord::new(20, 30));
@@ -280,7 +279,18 @@ mod tests {
         let left = nalgebra::Vector2::new(30, 40);
         let right = nalgebra::Vector2::new(10, 20);
 
-        assert_eq!(left - right, Coord::new(20, 20));
+        assert_eq!(Coord::from(left - right), Coord::new(20, 20));
+    }
+
+    #[test]
+    #[cfg(feature = "nalgebra_support")]
+    fn convert_ref() {
+        let left = nalgebra::Vector2::new(30, 40);
+        let right = nalgebra::Vector2::new(10, 20);
+
+        let c = left - right;
+
+        assert_eq!(Coord::from(&c), Coord::new(20, 20));
     }
 
     #[test]
--- embedded-graphics/src/unsignedcoord.rs
+++ embedded-graphics/src/unsignedcoord.rs
@@ -4,122 +4,117 @@ use crate::coord::Coord;
 
 type UnsignedCoordPart = u32;
 
-#[cfg(not(feature = "nalgebra_support"))]
-mod internal_unsigned_coord {
-    use super::UnsignedCoordPart;
-    use crate::coord::Coord;
-    use core::ops::{Add, AddAssign, Index, Neg, Sub, SubAssign};
+use core::ops::{Add, AddAssign, Index, Neg, Sub, SubAssign};
 
-    /// 2D unsigned coordinate in screen space
-    ///
-    /// As opposed to [`Coord`](../coord/index.html), this coordinate is unsigned. It is intended for
-    /// use with [`Drawable`](../drawable/trait.Drawable.html) iterators to output valid _display pixel_
-    /// coordinates, i.e. coordinates that are always positive.
-    ///
-    /// ```rust
-    /// use embedded_graphics::{unsignedcoord::UnsignedCoord, ucoord};
-    ///
-    /// // Create a coord using the `new` constructor method
-    /// let c1 = UnsignedCoord::new(10, 20);
-    ///
-    /// // Create a coord using the handy `ucoord` macro
-    /// let c2 = ucoord!(10, 20);
-    ///
-    /// assert_eq!(c1, c2);
-    /// ```
-    ///
-    /// Note that enabling the `nalgebra` feature will alias Nalgebra's [`Vector2<u32>`] type to
-    /// `UnsignedCoord` instead of this builtin implementation.
-    ///
-    /// [`Vector2<u32>`]: https://docs.rs/nalgebra/0.18.0/nalgebra/base/type.Vector2.html
-    #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-    pub struct UnsignedCoord(pub UnsignedCoordPart, pub UnsignedCoordPart);
+/// 2D unsigned coordinate in screen space
+///
+/// As opposed to [`Coord`](../coord/index.html), this coordinate is unsigned. It is intended for
+/// use with [`Drawable`](../drawable/trait.Drawable.html) iterators to output valid _display pixel_
+/// coordinates, i.e. coordinates that are always positive.
+///
+/// ```rust
+/// use embedded_graphics::{unsignedcoord::UnsignedCoord, ucoord};
+///
+/// // Create a coord using the `new` constructor method
+/// let c1 = UnsignedCoord::new(10, 20);
+///
+/// // Create a coord using the handy `ucoord` macro
+/// let c2 = ucoord!(10, 20);
+///
+/// assert_eq!(c1, c2);
+/// ```
+///
+/// Note that enabling the `nalgebra` feature will alias Nalgebra's [`Vector2<u32>`] type to
+/// `UnsignedCoord` instead of this builtin implementation.
+///
+/// [`Vector2<u32>`]: https://docs.rs/nalgebra/0.18.0/nalgebra/base/type.Vector2.html
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct UnsignedCoord(pub UnsignedCoordPart, pub UnsignedCoordPart);
 
-    impl UnsignedCoord {
-        /// Create a new coordinate with X and Y values
-        pub fn new(x: UnsignedCoordPart, y: UnsignedCoordPart) -> Self {
-            UnsignedCoord(x, y)
+impl UnsignedCoord {
+    /// Create a new coordinate with X and Y values
+    pub fn new(x: UnsignedCoordPart, y: UnsignedCoordPart) -> Self {
+        UnsignedCoord(x, y)
+    }
+}
+
+impl Add for UnsignedCoord {
+    type Output = UnsignedCoord;
+
+    fn add(self, other: UnsignedCoord) -> UnsignedCoord {
+        UnsignedCoord(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl AddAssign for UnsignedCoord {
+    fn add_assign(&mut self, other: UnsignedCoord) {
+        self.0 += other.0;
+        self.1 += other.1;
+    }
+}
+
+impl Sub for UnsignedCoord {
+    type Output = UnsignedCoord;
+
+    fn sub(self, other: UnsignedCoord) -> UnsignedCoord {
+        UnsignedCoord(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl SubAssign for UnsignedCoord {
+    fn sub_assign(&mut self, other: UnsignedCoord) {
+        self.0 -= other.0;
+        self.1 -= other.1;
+    }
+}
+
+impl Index<usize> for UnsignedCoord {
+    type Output = UnsignedCoordPart;
+
+    fn index(&self, idx: usize) -> &UnsignedCoordPart {
+        match idx {
+            0 => &self.0,
+            1 => &self.1,
+            _ => panic!("Unreachable index {}", idx),
         }
     }
+}
 
-    impl Add for UnsignedCoord {
-        type Output = UnsignedCoord;
+impl Neg for UnsignedCoord {
+    type Output = Coord;
 
-        fn add(self, other: UnsignedCoord) -> UnsignedCoord {
-            UnsignedCoord(self.0 + other.0, self.1 + other.1)
-        }
+    fn neg(self) -> Self::Output {
+        Coord::new(-(self.0 as i32), -(self.1 as i32))
     }
+}
 
-    impl AddAssign for UnsignedCoord {
-        fn add_assign(&mut self, other: UnsignedCoord) {
-            self.0 += other.0;
-            self.1 += other.1;
-        }
+impl From<(u32, u32)> for UnsignedCoord {
+    fn from(other: (u32, u32)) -> Self {
+        Self(other.0, other.1)
     }
+}
 
-    impl Sub for UnsignedCoord {
-        type Output = UnsignedCoord;
-
-        fn sub(self, other: UnsignedCoord) -> UnsignedCoord {
-            UnsignedCoord(self.0 - other.0, self.1 - other.1)
-        }
+impl From<[u32; 2]> for UnsignedCoord {
+    fn from(other: [u32; 2]) -> Self {
+        Self(other[0], other[1])
     }
+}
 
-    impl SubAssign for UnsignedCoord {
-        fn sub_assign(&mut self, other: UnsignedCoord) {
-            self.0 -= other.0;
-            self.1 -= other.1;
-        }
+impl From<&[u32; 2]> for UnsignedCoord {
+    fn from(other: &[u32; 2]) -> Self {
+        Self(other[0], other[1])
     }
+}
 
-    impl Index<usize> for UnsignedCoord {
-        type Output = UnsignedCoordPart;
-
-        fn index(&self, idx: usize) -> &UnsignedCoordPart {
-            match idx {
-                0 => &self.0,
-                1 => &self.1,
-                _ => panic!("Unreachable index {}", idx),
-            }
-        }
+impl From<UnsignedCoord> for (u32, u32) {
+    fn from(other: UnsignedCoord) -> (u32, u32) {
+        (other.0, other.1)
     }
+}
 
-    impl Neg for UnsignedCoord {
-        type Output = Coord;
-
-        fn neg(self) -> Self::Output {
-            Coord::new(-(self.0 as i32), -(self.1 as i32))
-        }
-    }
-
-    impl From<(u32, u32)> for UnsignedCoord {
-        fn from(other: (u32, u32)) -> Self {
-            Self(other.0, other.1)
-        }
-    }
-
-    impl From<[u32; 2]> for UnsignedCoord {
-        fn from(other: [u32; 2]) -> Self {
-            Self(other[0], other[1])
-        }
-    }
-
-    impl From<&[u32; 2]> for UnsignedCoord {
-        fn from(other: &[u32; 2]) -> Self {
-            Self(other[0], other[1])
-        }
-    }
-
-    impl From<UnsignedCoord> for (u32, u32) {
-        fn from(other: UnsignedCoord) -> (u32, u32) {
-            (other.0, other.1)
-        }
-    }
-
-    impl From<&UnsignedCoord> for (u32, u32) {
-        fn from(other: &UnsignedCoord) -> (u32, u32) {
-            (other.0, other.1)
-        }
+impl From<&UnsignedCoord> for (u32, u32) {
+    fn from(other: &UnsignedCoord) -> (u32, u32) {
+        (other.0, other.1)
     }
 }
 
@@ -139,15 +134,28 @@ macro_rules! ucoord {
     };
 }
 
-#[cfg(not(feature = "nalgebra_support"))]
-pub use self::internal_unsigned_coord::UnsignedCoord;
+#[cfg(feature = "nalgebra_support")]
+use nalgebra::{base::Scalar, Vector2};
 
 #[cfg(feature = "nalgebra_support")]
-use nalgebra;
+impl<N> From<Vector2<N>> for UnsignedCoord
+where
+    N: Into<UnsignedCoordPart> + Scalar,
+{
+    fn from(other: Vector2<N>) -> Self {
+        Self::new(other[0].into(), other[1].into())
+    }
+}
 
 #[cfg(feature = "nalgebra_support")]
-/// 2D coordinate type with Nalgebra support
-pub type UnsignedCoord = nalgebra::Vector2<UnsignedCoordPart>;
+impl<N> From<&Vector2<N>> for UnsignedCoord
+where
+    N: Into<UnsignedCoordPart> + Scalar,
+{
+    fn from(other: &Vector2<N>) -> Self {
+        Self::new(other[0].into(), other[1].into())
+    }
+}
 
 /// Convert an unsigned coordinate to a signed representation
 ///
@@ -185,7 +193,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(feature = "nalgebra_support"))]
     fn from_tuple() {
         assert_eq!(UnsignedCoord::from((20, 30)), UnsignedCoord::new(20, 30));
     }
@@ -196,7 +203,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(feature = "nalgebra_support"))]
     fn from_array_ref() {
         assert_eq!(UnsignedCoord::from(&[20, 30]), UnsignedCoord::new(20, 30));
     }
@@ -204,14 +210,16 @@ mod tests {
     #[test]
     #[cfg(feature = "nalgebra_support")]
     fn nalgebra_support() {
-        let left = nalgebra::Vector2::new(30, 40);
+        let left = nalgebra::Vector2::new(30u32, 40);
         let right = nalgebra::Vector2::new(10, 20);
 
-        assert_eq!(left - right, UnsignedCoord::new(20, 20));
+        assert_eq!(
+            UnsignedCoord::from(left - right),
+            UnsignedCoord::new(20, 20)
+        );
     }
 
     #[test]
-    #[cfg(not(feature = "nalgebra_support"))]
     fn neg() {
         assert_eq!(-UnsignedCoord::new(10, 20), Coord::new(-10, -20));
     }
