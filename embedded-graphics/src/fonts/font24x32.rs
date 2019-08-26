2f6fb4a4a5f7df6c1dec6db1bfea4896a588fe6f
Tyler Holmes <tylerdanielholmes@gmail.com>
2019-08-25 22:10:05 -0700

    add initial support for 24x32 font
    this is just a dumb doubling of the 12x16 font, so it's blocky. Haven't enabled the tests yet, will do in subsequent commit

---
+++ embedded-graphics/src/fonts/font24x32.rs
@@ -0,0 +1,255 @@
+use crate::fonts::font_builder::{FontBuilder, FontBuilderConf};
+
+#[derive(Debug, Copy, Clone)]
+pub enum Font24x32Conf {}
+impl FontBuilderConf for Font24x32Conf {
+    const FONT_IMAGE: &'static [u8] = include_bytes!("../../data/font24x32_1bpp.raw");
+    const CHAR_HEIGHT: u32 = 32;
+    const CHAR_WIDTH: u32 = 24;
+    const FONT_IMAGE_WIDTH: u32 = 480;
+    fn char_offset(c: char) -> u32 {
+        let fallback = '?' as u32 - ' ' as u32;
+        if c < ' ' {
+            return fallback;
+        }
+        if c <= '~' {
+            return c as u32 - ' ' as u32;
+        }
+        fallback
+    }
+}
+
+/// 24x32 pixel monospace font
+///
+/// There is also the [`text_24x32`] macro to provide an easier to use interface.
+///
+/// # Examples
+///
+/// ## Write some text to the screen at the default `(0, 0)` position
+///
+/// ```rust
+/// use embedded_graphics::prelude::*;
+/// use embedded_graphics::fonts::Font24x32;
+/// use embedded_graphics::text_24x32;
+/// # use embedded_graphics::mock_display::Display;
+/// # let mut display = Display::default();
+///
+/// // Use struct methods directly
+/// display.draw(Font24x32::render_str("Hello Rust!"));
+///
+/// // Use a macro instead
+/// display.draw(text_24x32!("Hello Rust!"));
+/// ```
+///
+/// ## Translate text by (20px, 30px)
+///
+/// ```rust
+/// use embedded_graphics::prelude::*;
+/// use embedded_graphics::fonts::Font24x32;
+/// # use embedded_graphics::mock_display::Display;
+/// # let mut display = Display::default();
+///
+/// display.draw(
+///     Font24x32::render_str("Hello Rust!").translate(Coord::new(20, 30))
+/// );
+/// ```
+///
+/// ## Add some styling to the text
+///
+/// Use [any method provided by the `WithStyle` trait](../style/trait.WithStyle.html#required-methods).
+/// Properties like `fill` or `stroke` passed to the `text_24x32` macro are converted into method
+/// calls verbatim.
+///
+/// ```rust
+/// use embedded_graphics::prelude::*;
+/// use embedded_graphics::text_24x32;
+/// use embedded_graphics::fonts::Font24x32;
+/// # use embedded_graphics::mock_display::Display;
+/// # let mut display = Display::default();
+///
+/// display.draw(text_24x32!(
+///     "Hello Rust!",
+///     fill = Some(1u8),
+///     stroke = Some(0u8)
+/// ));
+///
+/// display.draw(
+///     Font24x32::render_str("Hello Rust!")
+///         .translate(Coord::new(20, 30))
+///         .fill(Some(1u8))
+///         .stroke(Some(0u8)),
+/// );
+/// ```
+///
+/// [`text_24x32`]: ../macro.text_24x32.html
+pub type Font24x32<'a, C> = FontBuilder<'a, C, Font24x32Conf>;
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use crate::coord::Coord;
+//     use crate::drawable::Dimensions;
+//     use crate::fonts::Font;
+//     use crate::mock_display::Display;
+//     use crate::style::Style;
+//     use crate::style::WithStyle;
+//     use crate::transform::Transform;
+//     use crate::unsignedcoord::UnsignedCoord;
+//     use crate::Drawing;
+
+//     #[test]
+//     fn off_screen_text_does_not_infinite_loop() {
+//         let text: Font24x32<u8> = Font24x32::render_str("Hello World!")
+//             .translate(Coord::new(5, -20))
+//             .style(Style::stroke(1));
+//         let mut it = text.into_iter();
+
+//         assert_eq!(it.next(), None);
+//     }
+
+//     #[test]
+//     fn text_dimensions() {
+//         let hello: Font24x32<u8> = Font24x32::render_str("Hello World!");
+//         let empty: Font24x32<u8> = Font24x32::render_str("");
+
+//         assert_eq!(hello.size(), UnsignedCoord::new(144, 16));
+//         assert_eq!(empty.size(), UnsignedCoord::new(0, 0));
+//     }
+
+//     #[test]
+//     fn text_corners() {
+//         let hello: Font24x32<u8> =
+//             Font24x32::render_str("Hello World!").translate(Coord::new(5, -20));
+//         let empty: Font24x32<u8> = Font24x32::render_str("").translate(Coord::new(10, 20));
+
+//         assert_eq!(hello.top_left(), Coord::new(5, -20));
+//         assert_eq!(hello.bottom_right(), Coord::new(144 + 5, 16 - 20));
+//         assert_eq!(empty.top_left(), Coord::new(10, 20));
+//         assert_eq!(empty.bottom_right(), Coord::new(10, 20));
+//     }
+
+//     #[test]
+//     fn correct_m() {
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str("Mm").stroke(Some(1)));
+
+//         assert_eq!(
+//             display,
+//             Display::new([
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+//                 [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+//                 [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//             ])
+//         );
+//     }
+
+//     #[test]
+//     fn correct_ascii_borders() {
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str(" ~").stroke(Some(1)));
+
+//         assert_eq!(
+//             display,
+//             Display::new([
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//             ])
+//         );
+//     }
+
+//     #[test]
+//     fn correct_dollar_y() {
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str("$y").stroke(Some(1)));
+
+//         assert_eq!(
+//             display,
+//             Display::new([
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+//                 [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+//             ])
+//         );
+//     }
+
+//     #[test]
+//     fn dont_panic() {
+//         #[cfg_attr(rustfmt, rustfmt_skip)]
+//         let two_question_marks = Display::new(
+//             [
+//                 [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+//                 [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//             ]
+//         );
+
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str("\0\n").stroke(Some(1)));
+//         assert_eq!(display, two_question_marks);
+
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str("\x7F\u{A0}").stroke(Some(1)));
+//         assert_eq!(display, two_question_marks);
+
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str("¡ÿ").stroke(Some(1)));
+//         assert_eq!(display, two_question_marks);
+
+//         let mut display = Display::default();
+//         display.draw(Font24x32::render_str("Ā💣").stroke(Some(1)));
+//         assert_eq!(display, two_question_marks);
+//     }
+// }
--- embedded-graphics/src/fonts/mod.rs
+++ embedded-graphics/src/fonts/mod.rs
@@ -100,12 +100,14 @@
 //! [`ArrayString`]: https://docs.rs/arrayvec/0.4.11/arrayvec/struct.ArrayString.html
 //! [`write!()`]: https://doc.rust-lang.org/nightly/std/macro.write.html
 
+mod font24x32;
 mod font12x16;
 mod font6x12;
 mod font6x8;
 mod font8x16;
 pub mod font_builder;
 
+pub use self::font24x32::Font24x32;
 pub use self::font12x16::Font12x16;
 pub use self::font6x12::Font6x12;
 pub use self::font6x8::Font6x8;
