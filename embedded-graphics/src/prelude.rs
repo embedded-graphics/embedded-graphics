43b9995f53dcd611eef7ce32b48d4d87c12d6275
James Waples <james@wapl.es>
2019-09-02 22:13:06 +0100

    Add raw::RawData to prelude

--- embedded-graphics/src/prelude.rs
+++ embedded-graphics/src/prelude.rs
@@ -4,7 +4,7 @@ pub use super::drawable::Pixel;
 pub use super::fonts::Font;
 pub use super::geometry::{Dimensions, Point, Size};
 pub use super::image::ImageFile;
-pub use super::pixelcolor::{GrayColor, PixelColor, RgbColor};
+pub use super::pixelcolor::{raw::RawData, GrayColor, PixelColor, RgbColor};
 pub use super::style::{Style, WithStyle};
 pub use super::transform::Transform;
 pub use super::{Drawing, SizedDrawing};
