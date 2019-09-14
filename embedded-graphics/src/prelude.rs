e3412829cb16f19ec3d5a80a007e4f0b61132a47
Ralf Fuest <mail@rfuest.de>
2019-09-14 21:12:55 +0200

    Remove SizedDrawing trait (#180)

--- embedded-graphics/src/prelude.rs
+++ embedded-graphics/src/prelude.rs
@@ -7,4 +7,4 @@ pub use super::image::ImageFile;
 pub use super::pixelcolor::{raw::RawData, GrayColor, PixelColor, RgbColor};
 pub use super::style::{Style, WithStyle};
 pub use super::transform::Transform;
-pub use super::{Drawing, SizedDrawing};
+pub use super::Drawing;
