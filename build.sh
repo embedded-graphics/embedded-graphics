88a03d6a30f2c59b2a91c00de71be3b3944f0cfd
James Waples <jamwaffles@gmail.com>
2019-11-13 19:39:41 +0000

    Revert entire change from #187

--- build.sh
+++ build.sh
@@ -4,14 +4,10 @@ set -e
 
 cargo fmt --all -- --check
 cargo test --release
-
-pushd embedded-graphics
 cargo test --release --all-features
-popd
-
-cargo doc --all-features
 cargo bench --no-run
 
+cargo doc --all-features
 linkchecker target/doc/embedded_graphics/index.html
 linkchecker target/doc/tinybmp/index.html
 linkchecker target/doc/tinytga/index.html
