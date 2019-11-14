76fdabde430394610ea0929d026ba8456fdf1a82
James Waples <james@wapl.es>
2019-11-14 15:36:22 +0000

    Build docs in project root (#201)
    * Build docs in project root
    
    * Revert entire change from #187

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ set -e
 
 cargo fmt --all -- --check
 cargo test --release
-pushd embedded-graphics
 cargo test --release --all-features
-cargo doc --all-features
-popd
 cargo bench --no-run
 
+cargo doc --all-features
 linkchecker target/doc/embedded_graphics/index.html
 linkchecker target/doc/tinybmp/index.html
 linkchecker target/doc/tinytga/index.html
