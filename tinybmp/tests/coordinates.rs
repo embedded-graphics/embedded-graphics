3957404b6c53c00d50348c52ff55343de07e33e1
James Waples <james@wapl.es>
2020-02-17 11:32:19 +0000

    Remove redundant assertion

--- tinybmp/tests/coordinates.rs
+++ tinybmp/tests/coordinates.rs
@@ -7,9 +7,6 @@ fn coordinates() {
 
     let pixels: Vec<_> = bmp.into_iter().map(|Pixel { x, y, .. }| (x, y)).collect();
 
-    // 8px x 8px image. Check that iterator returns all pixels in it
-    assert_eq!(pixels.len(), 8 * 8);
-
     #[rustfmt::skip]
     let expected = vec![
         (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), //
