623bb932fb62c97cbb74aeecb764de0fa58e9bef
Ralf Fuest <mail@rfuest.de>
2020-02-21 17:07:50 +0100

    Add new hello world example

---
+++ simulator/examples/hello-world.rs
@@ -0,0 +1,61 @@
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, Rectangle, Triangle},
+    style::{PrimitiveStyle, TextStyle},
+};
+use embedded_graphics_simulator::{BinaryColorTheme, SimulatorDisplay, WindowBuilder};
+
+fn main() -> Result<(), std::convert::Infallible> {
+    // Create a new simulator display with 128x64 pixels.
+    let mut display = SimulatorDisplay::new(Size::new(128, 64));
+
+    // Create styles used by the drawing operations.
+    let thin_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
+    let thick_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 3);
+    let fill = PrimitiveStyle::with_fill(BinaryColor::On);
+    let text_style = TextStyle::new(Font6x8, BinaryColor::On);
+
+    let yoffset = 10;
+
+    // Draw an 3px wide outline around the display.
+    let bottom_right = Point::zero() + display.size() - Point::new(1, 1);
+    Rectangle::new(Point::zero(), bottom_right)
+        .into_styled(thick_stroke)
+        .draw(&mut display)?;
+
+    // Draw a triangle.
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+    .into_styled(thin_stroke)
+    .draw(&mut display)?;
+
+    // Draw a filled square
+    Rectangle::new(Point::new(52, yoffset), Point::new(52 + 16, 16 + yoffset))
+        .into_styled(fill)
+        .draw(&mut display)?;
+
+    // Draw a square with a 3px wide stroke.
+    Circle::new(Point::new(96, yoffset + 8), 8)
+        .into_styled(thick_stroke)
+        .draw(&mut display)?;
+
+    // Draw centered text.
+    let text = "embedded-graphics";
+    let width = text.len() as i32 * 6;
+    Text::new(text, Point::new(64 - width / 2, 40))
+        .into_styled(text_style)
+        .draw(&mut display)?;
+
+    let mut window = WindowBuilder::new(&display)
+        .title("Hello World")
+        .theme(BinaryColorTheme::OledBlue)
+        .build();
+    window.show_static(&display);
+
+    Ok(())
+}
