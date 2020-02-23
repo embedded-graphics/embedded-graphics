52e31a95f07aff6278a7cda8e84d62156abf3ec4
Ralf Fuest <mail@rfuest.de>
2020-02-23 16:35:05 +0100

    Only run tests in README.md on x86_64 targets

--- embedded-graphics/src/check_readme.rs
+++ embedded-graphics/src/check_readme.rs
@@ -1,5 +1,6 @@
 //! This file includes the contents of the project's README.md so the code examples in it can be run
-//! as Rust doc tests
+//! as Rust doc tests. The tests are only included on `x86_64` because they can't be executed on
+//! `no_std` targets.
 
 macro_rules! doc {
     ($e:expr) => {
@@ -8,4 +9,5 @@ macro_rules! doc {
     };
 }
 
+#[cfg(target_arch = "x86_64")]
 doc!(include_str!("../README.md"));
