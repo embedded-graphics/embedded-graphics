9792742490ed58efbc583b1e0e2914c4fef760f1
James Waples <james@wapl.es>
2020-03-10 11:02:41 +0000

    Slightly closer to correct thickness

--- embedded-graphics/src/primitives/perp_line.rs
+++ embedded-graphics/src/primitives/perp_line.rs
@@ -60,7 +60,7 @@ impl PerpLineIterator {
             e_square: 2 * dy,
             width: width,
             winit,
-            tk: dx + dy + winit,
+            tk: dx + dy,
             side: Side::Left,
             step_major,
             step_minor,
@@ -72,23 +72,10 @@ impl Iterator for PerpLineIterator {
     type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.tk > self.width as i32 {
-            // match self.side {
-            //     // Left side is complete, swap to right side now
-            //     Side::Left => {
-            //         self.tk = self.dx + self.dy - self.winit;
-            //         self.point = self.start;
-            //         self.error = self.initial_error;
-            //         self.side = Side::Right;
-
-            //         Self::next(self)
-            //     }
-
-            //     Side::Right => None,
-            // }
-            None
-        } else {
-            match self.side {
+        match self.side {
+            Side::Left if self.tk + self.winit > self.width as i32 => None,
+            Side::Right if self.tk - self.winit > self.width as i32 => None,
+            _ => match self.side {
                 Side::Left => {
                     let point = self.point_l;
 
@@ -107,7 +94,7 @@ impl Iterator for PerpLineIterator {
                     Some(point)
                 }
                 Side::Right => {
-                    if self.error_r > self.threshold {
+                    if self.error_r >= self.threshold {
                         self.point_r -= self.step_major;
                         self.error_r += self.e_diag;
                         self.tk += 2 * self.dy;
@@ -121,7 +108,59 @@ impl Iterator for PerpLineIterator {
 
                     Some(self.point_r)
                 }
-            }
+            },
         }
+
+        // if self.tk > self.width as i32 {
+        //     // match self.side {
+        //     //     // Left side is complete, swap to right side now
+        //     //     Side::Left => {
+        //     //         // self.tk = self.dx + self.dy - self.winit;
+        //     //         self.point_l = self.start;
+        //     //         self.error_l = self.initial_error;
+        //     //         self.side = Side::Right;
+
+        //     //         Self::next(self)
+        //     //     }
+
+        //     //     Side::Right => None,
+        //     // }
+        //     None
+        // } else {
+        //     match self.side {
+        //         Side::Left => {
+        //             let point = self.point_l;
+
+        //             if self.error_l > self.threshold {
+        //                 self.point_l += self.step_major;
+        //                 self.error_l += self.e_diag;
+        //                 self.tk += 2 * self.dy;
+        //             }
+
+        //             self.point_l -= self.step_minor;
+        //             self.error_l += self.e_square;
+        //             self.tk += 2 * self.dx;
+
+        //             self.side = Side::Right;
+
+        //             Some(point)
+        //         }
+        //         Side::Right => {
+        //             if self.error_r >= self.threshold {
+        //                 self.point_r -= self.step_major;
+        //                 self.error_r += self.e_diag;
+        //                 self.tk += 2 * self.dy;
+        //             }
+
+        //             self.point_r += self.step_minor;
+        //             self.error_r += self.e_square;
+        //             self.tk += 2 * self.dx;
+
+        //             self.side = Side::Left;
+
+        //             Some(self.point_r)
+        //         }
+        //     }
+        // }
     }
 }
