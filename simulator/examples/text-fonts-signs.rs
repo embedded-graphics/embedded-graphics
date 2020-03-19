00cb2bf5ffe8c09422b2bf643ae552bc18018218
Caemor <11088935+caemor@users.noreply.github.com>
2020-03-19 20:43:45 +0100

    Improved Example

--- simulator/examples/text-fonts-signs.rs
+++ simulator/examples/text-fonts-signs.rs
@@ -1,70 +1,46 @@
 use embedded_graphics::{
     egtext,
-    fonts::{Font12x16, Font6x12, Font6x6, Font6x8, Font8x16, Text},
+    fonts::{Text, *},
     pixelcolor::BinaryColor,
     prelude::*,
-    style::{TextStyle, TextStyleBuilder},
+    style::TextStyle,
     text_style,
 };
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
 
 fn main() -> Result<(), core::convert::Infallible> {
-    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(256, 144));
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(1200, 160));
+
+    let test_text  = "¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+    let test_text_with_line_break  = "¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ\nÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
 
     // Show smallest font with black font on white background (default value for fonts)
-    Text::new("Hell° World! - default style 6x8", Point::new(15, 15))
+    Text::new(test_text, Point::new(15, 15))
         .into_styled(TextStyle::new(Font6x8, BinaryColor::On))
         .draw(&mut display)?;
 
-    // Show smallest font with white font on black background
-    let style = TextStyleBuilder::new(Font6x8)
-        .text_color(BinaryColor::Off)
-        .background_color(BinaryColor::On)
-        .build();
-
-    Text::new("Hell° World! - inverse 6x8", Point::new(15, 30))
-        .into_styled(style)
-        .draw(&mut display)?;
-
-    // Show smallest font with white font on black background using a macro
-    egtext!(
-        text = "Hell° world! - inverse 6x8 with macro",
-        top_left = (15, 40),
-        style = text_style!(
-            font = Font6x8,
-            text_color = BinaryColor::On,
-            background_color = BinaryColor::Off
-        )
-    )
-    .draw(&mut display)?;
-
-    // Show 6x12 Font
-    Text::new("Hell° 6x12!", Point::new(15, 55))
+    // Doesn't support as much
+    // // Show 6x12 Font
+    Text::new("-- Font 6x12 not support!", Point::new(15, 30))
         .into_styled(TextStyle::new(Font6x12, BinaryColor::On))
         .draw(&mut display)?;
 
     // Show 8x16 Font
-    Text::new("Hell° 8x16!", Point::new(15, 80))
+    Text::new(test_text, Point::new(15, 45))
         .into_styled(TextStyle::new(Font8x16, BinaryColor::On))
         .draw(&mut display)?;
 
-    // Show 12x16 Font using a macro
-    egtext!(
-        text = "Hell° 12x16!",
-        top_left = (15, 105),
-        style = text_style!(font = Font12x16, text_color = BinaryColor::On)
-    )
-    .draw(&mut display)?;
+    // Show 12x16 Font
+    Text::new(test_text, Point::new(15, 65))
+        .into_styled(TextStyle::new(Font12x16, BinaryColor::On))
+        .draw(&mut display)?;
 
-    // Show 6x6 Font using a macro
-    egtext!(
-        text = "Hell° 6x6!",
-        top_left = (15, 128),
-        style = text_style!(font = Font6x6, text_color = BinaryColor::On)
-    )
-    .draw(&mut display)?;
+    // Show 24x32 Font
+    Text::new(test_text_with_line_break, Point::new(15, 85))
+        .into_styled(TextStyle::new(Font24x32, BinaryColor::On))
+        .draw(&mut display)?;
 
-    let output_settings = OutputSettingsBuilder::new().scale(2).build();
+    let output_settings = OutputSettingsBuilder::new().scale(1).build();
     Window::new("Fonts", &output_settings).show_static(&display);
 
     Ok(())
