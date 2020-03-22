1a95edbdbd1d93330fb755ddd2dd66270abb61fb
James Waples <james@wapl.es>
2020-03-22 12:10:34 +0000

    Move over to macro magic from rfuest

--- simulator/src/bin/generate-example-screenshots.rs
+++ simulator/src/bin/generate-example-screenshots.rs
@@ -1,74 +1,71 @@
 //! Generate example screenshots for the embedded-graphics documentation
 //!
-//! Run from the workspace root with `cargo run --bin generate-example-screenshots`.
+//! Run from the workspace root with:
+//!
+//! ```bash
+//! cargo run --bin generate-example-screenshots | rustfmt +nightly --config-path rustfmt.nightly.toml
+//! ```
 //!
 //! Screenshots are output to `target/drawing-ops`.
 
-use eg::pixelcolor::RgbColor;
-use embedded_graphics as eg;
-use embedded_graphics::DrawTarget;
+use embedded_graphics::{
+    pixelcolor::Rgb888, prelude::*, primitives::Rectangle, style::PrimitiveStyleBuilder,
+};
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay};
-use std::collections::HashMap;
 
-fn main() -> Result<(), core::convert::Infallible> {
-    let output_base = "./target/drawing-ops";
+const OUTPUT_BASE: &str = "./target/drawing-ops";
 
-    std::fs::create_dir_all(output_base).expect("Output directory could not be created");
+macro_rules! op {
+    ($display:ident, $name:expr, $code:block) => {
+        $display.clear(Rgb888::BLACK).unwrap();
 
-    let mut display: SimulatorDisplay<_> = SimulatorDisplay::new(eg::geometry::Size::new(64, 64));
+        (|| $code)().unwrap();
 
-    let output_settings = OutputSettingsBuilder::new()
-        .scale(2)
-        .pixel_spacing(1)
-        .build();
+        let output_settings = OutputSettingsBuilder::new()
+            .scale(2)
+            .pixel_spacing(1)
+            .build();
 
-    let mut examples: HashMap<
-        &'static str,
-        Box<dyn Fn(&mut SimulatorDisplay<_>) -> Result<(), core::convert::Infallible>>,
-    > = HashMap::new();
-
-    // Add examples
-    examples.insert(
-        "pixel",
-        Box::new(|display: &mut SimulatorDisplay<_>| {
-            use embedded_graphics::{pixelcolor::Rgb888, prelude::*};
-
-            Pixel(Point::new(32, 32), Rgb888::RED).draw(display)
-        }),
-    );
-
-    examples.insert(
-        "rectangle",
-        Box::new(|display: &mut SimulatorDisplay<_>| {
-            use embedded_graphics::{
-                pixelcolor::Rgb888, prelude::*, primitives::Rectangle, style::PrimitiveStyleBuilder,
-            };
-
-            Rectangle::new(Point::new(16, 24), Point::new(48, 40))
-                .into_styled(
-                    PrimitiveStyleBuilder::new()
-                        .stroke_width(2)
-                        .stroke_color(Rgb888::RED)
-                        .fill_color(Rgb888::CYAN)
-                        .build(),
-                )
-                .draw(display)
-        }),
-    );
-
-    // Render all examples to images
-    for (name, code) in examples {
-        display.clear(eg::pixelcolor::Rgb888::BLACK)?;
-
-        let path = format!("{}/{}.png", output_base, name);
-
-        code(&mut display)?;
-
-        display
+        let path = format!("{}/{}.png", OUTPUT_BASE, $name);
+        $display
             .to_image_buffer(&output_settings)
             .save(path)
             .unwrap();
-    }
 
-    Ok(())
+        let docs = format!(
+            "```rust\n{}\n```",
+            stringify!($code).trim_matches(|c| c == '{' || c == '}')
+        );
+
+        println!("/// # {} example", $name);
+        for line in docs.lines() {
+            println!("/// {}", line);
+        }
+    };
+}
+
+fn main() {
+    let output_base = "./target/drawing-ops";
+    std::fs::create_dir_all(output_base).expect("Output directory could not be created");
+
+    let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(64, 64));
+
+    op!(display, "pixel", {
+        Pixel(Point::new(32, 32), Rgb888::RED).draw(&mut display)
+    });
+
+    op!(display, "rectangle", {
+        Rectangle::new(Point::new(16, 24), Point::new(48, 40))
+            .into_styled(
+                PrimitiveStyleBuilder::new()
+                    .stroke_width(2)
+                    .stroke_color(Rgb888::RED)
+                    .fill_color(Rgb888::CYAN)
+                    .build(),
+            )
+            .draw(&mut display)
+    });
+
+    // Add dummy mod to allow running rustfmt
+    println!("mod dummy {}", "{}");
 }
