a2b30ea15dbcfe2e6b848049c2900bed60dfc0c5
James Waples <james@wapl.es>
2020-05-20 12:13:56 +0100

    Add ellipse to primitives-fill-macros demo

--- simulator/examples/primitives-fill-macros.rs
+++ simulator/examples/primitives-fill-macros.rs
@@ -4,14 +4,16 @@
 //! same as the `primitives-fill` example.
 
 use embedded_graphics::{
-    egcircle, egrectangle, egtriangle, pixelcolor::BinaryColor, prelude::*, primitive_style,
+    egcircle, egellipse, egrectangle, egtriangle, pixelcolor::BinaryColor, prelude::*,
+    primitive_style,
 };
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
 
 static CIRCLE_SIZE: i32 = 65;
+static ELLIPSE_SIZE: Size = Size::new(90, 65);
 
 fn main() -> Result<(), core::convert::Infallible> {
-    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(384, 128));
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(404, 128));
 
     egcircle!(
         top_left = (0, 0),
@@ -103,6 +105,38 @@ fn main() -> Result<(), core::convert::Infallible> {
         )
     )
     .translate(Point::new(96 * 2 + 32, 32))
+    .draw(&mut display)?;
+
+    egellipse!(
+        top_left = Point::new(0, 0),
+        size = ELLIPSE_SIZE,
+        style = primitive_style!(stroke_color = BinaryColor::On, stroke_width = 1)
+    )
+    .translate(Point::new(96 * 3, 0))
+    .draw(&mut display)?;
+
+    egellipse!(
+        top_left = Point::new(0, 0),
+        size = ELLIPSE_SIZE,
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::On,
+        )
+    )
+    .translate(Point::new(96 * 3 + 16, 16))
+    .draw(&mut display)?;
+
+    egellipse!(
+        top_left = Point::new(0, 0),
+        size = ELLIPSE_SIZE,
+        style = primitive_style!(
+            stroke_color = BinaryColor::Off,
+            stroke_width = 1,
+            fill_color = BinaryColor::Off,
+        )
+    )
+    .translate(Point::new(96 * 3 + 32, 32))
     .draw(&mut display)?;
 
     let output_settings = OutputSettingsBuilder::new().scale(2).build();
