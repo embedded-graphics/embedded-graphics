be2d75c489baaabc738287ee328f12938fc4cc3e
James Waples <james@wapl.es>
2020-06-02 14:34:11 +0100

    Fix large stroke/small rect issues

--- embedded-graphics/src/primitives/rectangle.rs
+++ embedded-graphics/src/primitives/rectangle.rs
@@ -410,11 +410,6 @@ where
     C: PixelColor,
 {
     fn draw<D: DrawTarget<Color = C>>(self, display: &mut D) -> Result<(), D::Error> {
-        // Noop if rectangle is zero sized
-        if self.primitive.size == Size::zero() {
-            return Ok(());
-        }
-
         let fill_area = self.primitive.shrink(self.style.inside_stroke_width());
 
         // Fill rectangle
@@ -430,28 +425,47 @@ where
 
             let top_border = Rectangle::new(
                 stroke_area.top_left,
-                Size::new(stroke_area.size.width, stroke_width),
+                Size::new(
+                    stroke_area.size.width,
+                    stroke_width.min(stroke_area.size.height / 2),
+                ),
             );
 
-            let bottom_border = top_border.translate(Point::new(
-                0,
-                fill_area.size.height.saturating_add(top_border.size.height) as i32,
-            ));
+            let bottom_stroke_width =
+                stroke_width.min(stroke_area.size.height - top_border.size.height);
 
-            let left_border = Rectangle::new(
-                stroke_area.top_left + Size::new(0, top_border.size.height),
-                Size::new(stroke_width, fill_area.size.height),
+            let bottom_border = Rectangle::new(
+                top_border.top_left
+                    + Size::new(
+                        0,
+                        stroke_area.size.height.saturating_sub(bottom_stroke_width),
+                    ),
+                Size::new(stroke_area.size.width, bottom_stroke_width),
             );
-
-            let right_border = left_border.translate(Point::new(
-                fill_area.size.width.saturating_add(stroke_width) as i32,
-                0,
-            ));
 
             display.fill_solid(&top_border, stroke_color)?;
             display.fill_solid(&bottom_border, stroke_color)?;
-            display.fill_solid(&left_border, stroke_color)?;
-            display.fill_solid(&right_border, stroke_color)?;
+
+            if fill_area.size.height > 0 {
+                let left_border = Rectangle::new(
+                    stroke_area.top_left + top_border.size.y_axis(),
+                    Size::new(
+                        (stroke_width * 2).min(stroke_area.size.width + 1) / 2,
+                        fill_area.size.height,
+                    ),
+                );
+
+                let right_border = left_border.translate(Point::new(
+                    stroke_area
+                        .size
+                        .width
+                        .saturating_sub(left_border.size.width) as i32,
+                    0,
+                ));
+
+                display.fill_solid(&left_border, stroke_color)?;
+                display.fill_solid(&right_border, stroke_color)?;
+            }
         }
 
         Ok(())
@@ -464,6 +478,7 @@ mod tests {
     use crate::{
         mock_display::MockDisplay,
         pixelcolor::{BinaryColor, Rgb565, RgbColor},
+        prelude::*,
         style::{PrimitiveStyle, PrimitiveStyleBuilder, StrokeAlignment},
     };
 
@@ -732,5 +747,69 @@ mod tests {
         let rect2 = Rectangle::with_corners(Point::new_equal(5), Point::new(30, 40));
 
         assert_eq!(rect1.intersection(&rect2), rect1);
+    }
+
+    /// Compare the output of the draw() call vs iterators across multiple styles and stroke
+    /// alignments.
+    fn compare_drawable_iter(rect: Rectangle) {
+        let thin_stroke = PrimitiveStyle::with_stroke(Rgb565::RED, 1);
+        let stroke = PrimitiveStyle::with_stroke(Rgb565::RED, 5);
+        let stroke_fill = PrimitiveStyleBuilder::new()
+            .stroke_color(Rgb565::RED)
+            .stroke_width(5)
+            .fill_color(Rgb565::GREEN)
+            .build();
+        let fill = PrimitiveStyle::with_fill(Rgb565::BLUE);
+
+        for (name, style) in [
+            ("thin_stroke", thin_stroke),
+            ("stroke", stroke),
+            ("stroke_fill", stroke_fill),
+            ("fill", fill),
+        ]
+        .iter()
+        {
+            for alignment in [
+                StrokeAlignment::Center,
+                StrokeAlignment::Inside,
+                StrokeAlignment::Outside,
+            ]
+            .iter()
+            {
+                let style = PrimitiveStyleBuilder::from(style)
+                    .stroke_alignment(*alignment)
+                    .build();
+
+                let mut display_drawable = MockDisplay::new();
+                let mut display_iter = MockDisplay::new();
+
+                // Calls draw() impl above using fill_solid()
+                rect.into_styled(style).draw(&mut display_drawable).unwrap();
+
+                // Calls draw_iter()
+                rect.into_styled(style)
+                    .into_iter()
+                    .draw(&mut display_iter)
+                    .unwrap();
+
+                assert_eq!(
+                    display_drawable, display_iter,
+                    "{} x {} rectangle with style '{}' and alignment {:?} does not match iterator",
+                    rect.size.width, rect.size.height, name, alignment
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn drawable_vs_iterator() {
+        compare_drawable_iter(Rectangle::new(Point::new(10, 20), Size::new(20, 30)))
+    }
+
+    #[test]
+    fn drawable_vs_iterator_squares() {
+        for i in 0..20 {
+            compare_drawable_iter(Rectangle::new(Point::new(7, 7), Size::new_equal(i)))
+        }
     }
 }
