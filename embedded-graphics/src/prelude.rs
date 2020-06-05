b44104469056f63b5215c04bd36bc9cee5dafefe
James Waples <james@wapl.es>
2020-06-05 11:58:35 +0100

    Add ContainsPoint to prelude

--- embedded-graphics/src/prelude.rs
+++ embedded-graphics/src/prelude.rs
@@ -6,7 +6,7 @@ pub use crate::{
     geometry::{Dimensions, Point, Size},
     image::{ImageDimensions, IntoPixelIter},
     pixelcolor::{raw::RawData, GrayColor, IntoStorage, PixelColor, RgbColor},
-    primitives::Primitive,
+    primitives::{ContainsPoint, Primitive},
     transform::Transform,
 };
 
