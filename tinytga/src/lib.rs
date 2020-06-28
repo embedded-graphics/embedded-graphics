ad46af3ca8b52dcca82e8245293453e19adebbe9
James Waples <james@wapl.es>
2020-06-28 15:03:38 +0100

    Add examples and better tinytga crate description

--- tinytga/src/lib.rs
+++ tinytga/src/lib.rs
@@ -1,4 +1,80 @@
-//! No-std compatible TGA parser designed for embedded systems, but usable anywhere
+//! A small TGA parser designed for embedded, no-std environments but usable anywhere. Beyond
+//! parsing the image header, no other allocations are made. A reference to the input image data is
+//! kept and slices are returned from it.
+//!
+//! Call `Tga.into_iter()` to get an iterator over individual pixels in the image.
+//!
+//! # Examples
+//!
+//! ## Load a Run Length Encoded (RLE) TGA image
+//!
+//! ```rust
+//! use tinytga::{ImageType, Pixel, Tga, TgaFooter, TgaHeader};
+//!
+//! // Include an image from a local path as bytes
+//! let data = include_bytes!("../tests/chessboard_4px_rle.tga");
+//!
+//! // Create a TGA instance from a byte slice
+//! let img = Tga::from_slice(data).unwrap();
+//!
+//! // Take a look at the header
+//! assert_eq!(
+//!     img.header,
+//!     TgaHeader {
+//!         id_len: 0,
+//!         has_color_map: false,
+//!         image_type: ImageType::RleTruecolor,
+//!         color_map_start: 0,
+//!         color_map_len: 0,
+//!         color_map_depth: 0,
+//!         x_origin: 0,
+//!         y_origin: 4,
+//!         width: 4,
+//!         height: 4,
+//!         pixel_depth: 24,
+//!         image_descriptor: 32,
+//!     }
+//! );
+//!
+//! // Take a look at the footer
+//! assert_eq!(
+//!     img.footer,
+//!     Some(TgaFooter {
+//!         extension_area_offset: 0,
+//!         developer_directory_offset: 0
+//!     })
+//! );
+//!
+//! // Collect pixels into a `Vec<Pixel>`
+//! let pixels = img.into_iter().collect::<Vec<Pixel>>();
+//! ```
+//!
+//! ## Use with `embedded-graphics`
+//!
+//! This example demonstrates [`embedded-graphics`] support by rendering a TGA image to a mock
+//! display.
+//!
+//! The `graphics` feature of `tinytga` is required to enable embedded-graphics support.
+//!
+//! ```rust
+//! # #[cfg(feature = "graphics")] { fn main() -> Result<(), core::convert::Infallible> {
+//! # let mut display = embedded_graphics::mock_display::MockDisplay::default();
+//! use embedded_graphics::{image::Image, pixelcolor::Rgb888, prelude::*};
+//! use tinytga::Tga;
+//!
+//! let tga = Tga::from_slice(include_bytes!("../tests/rust-rle-bw-topleft.tga")).unwrap();
+//!
+//! let image: Image<Tga, Rgb888> = Image::new(&tga, Point::zero());
+//!
+//! image.draw(&mut display)?;
+//! # Ok::<(), core::convert::Infallible>(()) } }
+//! ```
+//!
+//! # Features
+//!
+//! * `graphics` - enables [`embedded-graphics`] integration.
+//!
+//! [`embedded-graphics`]: https://docs.rs/embedded-graphics
 
 #![no_std]
 #![deny(missing_docs)]
