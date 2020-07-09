bfd96fce52d466b38bb36a4061d9bb2af2a178cf
James Waples <james@wapl.es>
2020-07-09 10:59:47 +0100

    Add line segment intersection method and demo

---
+++ simulator/examples/line-intersection.rs
@@ -0,0 +1,79 @@
+use embedded_graphics::{pixelcolor::Rgb888, prelude::*, primitives::*, style::PrimitiveStyle};
+use embedded_graphics_simulator::{
+    OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+};
+
+fn draw(e2: Point, display: &mut SimulatorDisplay<Rgb888>) {
+    display.clear(Rgb888::BLACK).unwrap();
+
+    let s1 = Point::new(10, 100);
+    let e1 = Point::new(130, 40);
+
+    let s2 = Point::new(50, 10);
+
+    // First static line
+    let l1 = Line::new(s1, e1);
+
+    l1.into_styled(PrimitiveStyle::with_stroke(Rgb888::RED, 1))
+        .draw(display)
+        .unwrap();
+
+    // Second movable line
+    let l2 = Line::new(s2, e2);
+
+    l2.into_styled(PrimitiveStyle::with_stroke(Rgb888::GREEN, 1))
+        .draw(display)
+        .unwrap();
+
+    // Draw intersection point
+    if let Some(i) = l1.segment_intersection(&l2) {
+        Circle::with_center(i, 5)
+            .into_styled(PrimitiveStyle::with_fill(Rgb888::CYAN))
+            .draw(display)
+            .unwrap()
+    }
+}
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(200, 200));
+    let output_settings = OutputSettingsBuilder::new()
+        .scale(4)
+        .pixel_spacing(1)
+        .build();
+    let mut window = Window::new("Rounded rectangle debugger", &output_settings);
+
+    let mut end_point = Point::new(20, 20);
+
+    let mut mouse_down = false;
+
+    draw(end_point, &mut display);
+
+    'running: loop {
+        window.update(&display);
+
+        for event in window.events() {
+            match event {
+                SimulatorEvent::Quit => break 'running,
+                SimulatorEvent::MouseButtonDown { point, .. } => {
+                    mouse_down = true;
+
+                    end_point = point;
+                }
+                // SimulatorEvent::KeyDown { keycode, .. } => match keycode {
+                //     _ => (),
+                // },
+                SimulatorEvent::MouseButtonUp { .. } => mouse_down = false,
+                SimulatorEvent::MouseMove { point, .. } => {
+                    if mouse_down {
+                        end_point = point;
+                    }
+                }
+                _ => {}
+            }
+
+            draw(end_point, &mut display);
+        }
+    }
+
+    Ok(())
+}
