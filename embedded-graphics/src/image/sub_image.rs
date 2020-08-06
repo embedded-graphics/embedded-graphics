6948d07ac6acf996bed2b5d4ab49f31376db64c5
Ralf Fuest <mail@rfuest.de>
2020-08-06 16:33:47 +0200

    Add example link to SubImage docs

--- embedded-graphics/src/image/sub_image.rs
+++ embedded-graphics/src/image/sub_image.rs
@@ -11,10 +11,12 @@ use crate::{
 /// A sub image is rectangular subsection of an [`ImageDrawable`]. It can, for example, be used to
 /// draw individual sprites from a larger sprite atlas.
 ///
-/// To create a sub image call the [`sub_image`] method on the parent [`ImageDrawable`].
+/// To create a sub image call the [`sub_image`] method on the parent [`ImageDrawable`]. See the
+/// [module-level documentation] for an example.
 ///
 /// [`ImageDrawable`]: trait.ImageDrawable.html
 /// [`sub_image`]: trait.ImageDrawableExt.html#tymethod.sub_image
+/// [module-level documentation]: index.html#sub-images
 #[derive(Debug)]
 pub struct SubImage<'a, T> {
     parent: &'a T,
