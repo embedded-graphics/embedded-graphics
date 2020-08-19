35088b23a016596d9562cc3f821e1c246e5a7fb1
Dániel Buga <bugadani@gmail.com>
2020-08-20 00:11:50 +0200

    Add test cases for triangle contains

--- embedded-graphics/src/mock_display.rs
+++ embedded-graphics/src/mock_display.rs
@@ -186,7 +186,8 @@ use core::{
     iter,
 };
 
-const SIZE: usize = 64;
+// TODO FIXME restore this to 64
+const SIZE: usize = 128;
 const DISPLAY_AREA: Rectangle = Rectangle::new(Point::zero(), Size::new_equal(SIZE as u32));
 
 /// Mock display struct
