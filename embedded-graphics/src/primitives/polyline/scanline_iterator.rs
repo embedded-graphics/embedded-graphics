a50fedd1908001e5f5654de9d83ecf9da4bb90d4
Ralf Fuest <mail@rfuest.de>
2020-10-31 17:43:49 +0100

    Remove stop flag from ScanlineIterator

--- embedded-graphics/src/primitives/polyline/scanline_iterator.rs
+++ embedded-graphics/src/primitives/polyline/scanline_iterator.rs
@@ -15,7 +15,6 @@ pub struct ScanlineIterator<'a> {
     scanline_y: i32,
     scanline_limit: i32,
     intersections: ScanlineIntersections<'a>,
-    stop: bool,
 }
 
 impl<'a> ScanlineIterator<'a> {
@@ -32,7 +31,7 @@ impl<'a> ScanlineIterator<'a> {
         let bb = styled.bounding_box();
 
         let scanline_y = bb.top_left.y;
-        let scanline_limit = bb.bottom_right().map(|br| br.y).unwrap_or(scanline_y);
+        let scanline_limit = scanline_y + bb.size.height as i32;
 
         let intersections = ScanlineIntersections::new(
             styled.primitive.vertices,
@@ -45,7 +44,6 @@ impl<'a> ScanlineIterator<'a> {
             scanline_y,
             scanline_limit,
             intersections,
-            stop: scanline_y == scanline_limit,
         }
     }
 }
@@ -55,12 +53,8 @@ impl<'a> Iterator for ScanlineIterator<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if self.stop {
+            if self.scanline_y >= self.scanline_limit {
                 break None;
-            }
-
-            if self.scanline_y == self.scanline_limit {
-                self.stop = true;
             }
 
             if let Some(next) = self.intersections.next() {
@@ -69,8 +63,6 @@ impl<'a> Iterator for ScanlineIterator<'a> {
                 self.scanline_y += 1;
 
                 self.intersections.reset_with_new_scanline(self.scanline_y);
-
-                continue;
             }
         }
     }
