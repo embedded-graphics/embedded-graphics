deaafae42b75cd336b698261ed3b70b3469163a8
James Waples <james@wapl.es>
2020-11-22 17:39:51 +0000

    Add readme/changelog/docs skeleton

---
+++ embedded-graphics-core/CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+`embedded-graphics-core` changelog.
+
+<!-- next-header -->
+
+## [Unreleased] - ReleaseDate
+
+### Added
+
+- [#000](https://github.com/embedded-graphics/embedded-graphics/pull/000) Split common functionality out of `embedded-graphics` into `embedded-graphics-core`.
+
+
+<!-- next-url -->
+
+[unreleased]: https://github.com/embedded-graphics/embedded-graphics/compare/embedded-graphics-v0.7.0-alpha.1...HEAD
--- embedded-graphics-core/src/lib.rs
+++ embedded-graphics-core/src/lib.rs
@@ -1,3 +1,21 @@
+//! Embedded Graphics Core
+//!
+//! DOC TODO
+
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/embedded-graphics/embedded-graphics/191fe7f8a0fedc713f9722b9dc59208dacadee7e/assets/logo.svg?sanitize=true"
+)]
+#![no_std]
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_copy_implementations)]
+#![deny(trivial_casts)]
+#![deny(trivial_numeric_casts)]
+#![deny(unsafe_code)]
+#![deny(unstable_features)]
+#![deny(unused_import_braces)]
+#![deny(unused_qualifications)]
+
 pub mod draw_target;
 pub mod drawable;
 pub mod geometry;
