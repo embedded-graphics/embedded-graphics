ded319bf37f55a4385303d947baa41f851e76c1c
James Waples <james@wapl.es>
2020-11-28 15:44:40 +0000

    Add start of better docs to Dimensions

--- core/src/geometry/mod.rs
+++ core/src/geometry/mod.rs
@@ -10,14 +10,43 @@ use crate::primitives::Rectangle;
 
 /// Adds the ability to get the dimensions/position of an item.
 ///
-/// TODO
+/// This trait is implemented for many builtin items in embedded-graphics:
+///
+/// * Primitives - returns a rectangle that contains the mathematical representation of the shape.
+/// * Styled primitives - returns a rectangle encompassing all drawn pixels, including stroke
+///   thickness/offset.
+/// * [`DrawTarget`]s (displays, simulator, etc) - returns a rectangle encompassing the drawable
+///   display area with the origin at the top left (`0, 0`).
+/// * [`Image`]s - returns a rectangle with the exact width and height of the image.
+/// * [`Text`] - returns a rectangle containing all pixels required to draw the given text with the
+///   chosen font.
 ///
 /// # Implementation notes
 ///
-/// This trait must be implemented for any item that has a known size and position. For items where
-/// only the size is known, see the [`OriginDimensions`] trait.
+/// An implementation of `Dimensions` must produce a rectangle encompassing the entire item. For
+/// shapes, images and other drawable items, this means the rectangle must contain all pixels
+/// required to draw the item.
 ///
+/// [`DrawTarget`]s  (display drivers, etc) require that `Dimensions` is also implemented. The
+/// implementation must return the a rectangle representing the entire visible area of the display
+/// with the top left coordinate at the origin (`0, 0`). It is recommended that [`OriginDimensions`]
+/// is also implemented for draw targets.
+///
+/// Some traits in embedded-graphics-core require `Dimensions` to be implemented as well:
+///
+/// * [`DrawTarget`]: The implementation should return the drawable display area with the top left
+///   coordinate at the origin (`0, 0`).
+///
+/// When testing `Dimensions` implementations for [`Drawable`] items, [`MockDisplay::affected_area`]
+/// can be a useful method of checking that the bounding box is the correct size.
+///
+/// [`Drawable`]: ../drawable/trait.Drawable.html
+/// [`DrawTarget`]: ../draw_target/trait.DrawTarget.html
 /// [`OriginDimensions`]: trait.OriginDimensions.html
+/// [`Rectangle`]: ../primitives/rectangle/struct.Rectangle.html
+/// [`MockDisplay::affected_area`]: https://docs.rs/embedded-graphics/latest/embedded_graphics/mock_display/struct.MockDisplay.html#method.affected_area
+/// [`Image`]: https://docs.rs/embedded-graphics/latest/embedded_graphics/image/struct.Image.html
+/// [`Text`]: https://docs.rs/embedded-graphics/latest/embedded_graphics/fonts/struct.Text.html
 pub trait Dimensions {
     /// Returns the bounding box.
     fn bounding_box(&self) -> Rectangle;
@@ -26,6 +55,12 @@ pub trait Dimensions {
 /// Dimensions with `top_left` of the bounding box at `(0, 0)`.
 ///
 /// A blanket implementation of `Dimensions` is provided for all types that implement this trait.
+///
+/// # Implementation notes
+///
+/// `OriginDimensions` must be implemented for [`ImageDrawable`]s.
+///
+/// [`ImageDrawable`]: ../image/trait.ImageDrawable.html
 pub trait OriginDimensions {
     /// Returns the size of the bounding box.
     fn size(&self) -> Size;
