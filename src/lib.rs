5980a7e6324814f4778eb53a1cf9b5154aa9f8f3
James Waples <james@wapl.es>
2020-12-28 20:45:26 +0000

    Add Ableton Push2 to driver list (#521)

--- src/lib.rs
+++ src/lib.rs
@@ -63,6 +63,7 @@
 //! * [hub75](https://crates.io/crates/hub75): A rust driver for hub75 rgb matrix displays
 //! * [ili9341](https://crates.io/crates/ili9341): A platform agnostic driver to interface with the ILI9341 (and ILI9340C) TFT LCD display
 //! * [ls010b7dh01](https://crates.io/crates/ls010b7dh01): A platform agnostic driver for the LS010B7DH01 memory LCD display
+//! * [push2_display](https://crates.io/crates/push2_display): Ableton Push2 embedded-graphics display driver
 //! * [sh1106](https://crates.io/crates/sh1106): I2C driver for the SH1106 OLED display
 //! * [ssd1306](https://crates.io/crates/ssd1306): I2C and SPI (4 wire) driver for the SSD1306 OLED display
 //! * [ssd1309](https://crates.io/crates/ssd1309): I2C/SPI driver for the SSD1309 OLED display written in 100% Rust.
