623856a1277b65c6ab55904cf813fa25ce9be31b
Peter Stuart <peter@peterstuart.org>
2021-12-30 09:17:45 -0500

    Point::component_mul, component_div

--- core/src/geometry/point.rs
+++ core/src/geometry/point.rs
@@ -196,7 +196,7 @@ impl Point {
     ///
     /// assert_eq!(result, Point::new(-40, 90));
     /// ```
-    pub fn component_mul(self, other: Self) -> Self {
+    pub const fn component_mul(self, other: Self) -> Self {
         Self::new(self.x * other.x, self.y * other.y)
     }
 
@@ -213,7 +213,7 @@ impl Point {
     ///
     /// assert_eq!(result, Point::new(2, -10));
     /// ```
-    pub fn component_div(self, other: Self) -> Self {
+    pub const fn component_div(self, other: Self) -> Self {
         Self::new(self.x / other.x, self.y / other.y)
     }
 }
