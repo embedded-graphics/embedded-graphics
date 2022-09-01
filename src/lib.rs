ac215917903b96e29adb53ff8b71f1e51537978b
Finomnis <finomnis@gmail.com>
2022-09-01 12:44:37 +0200

    Make it clear that u8g2-fonts is an external font renderer

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@
 //! * [BMP images - `tinybmp`](https://crates.io/crates/tinybmp)
 //! * [TGA images - `tinytga`](https://crates.io/crates/tinytga)
 //! * [QOI images - `tinyqoi`](https://crates.io/crates/tinyqoi)
-//! * [Large collection of fonts - `u8g2-fonts`](https://crates.io/crates/u8g2-fonts)
+//! * [External font renderer with large collection of fonts - `u8g2-fonts`](https://crates.io/crates/u8g2-fonts)
 //! * [ProFont monospace font - `profont`](https://crates.io/crates/profont)
 //! * [Picofont Pico8 font - `embedded-picofont`](https://crates.io/crates/embedded_picofont)
 //! * [IBM437 font - `ibm437`](https://crates.io/crates/ibm437)
