1fd8fe48b74d088856105ea8139cf8527d3cb67a
James Waples <james@wapl.es>
2022-09-20 11:13:56 +0100

    Remove TODO
    It looks like the underlying implementation would be difficult to
    optimise, but that's just a hunch - I haven't profiled anything to see
    where the bottlenecks are.

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -126,7 +126,6 @@ where
     /// Get pixel color.
     ///
     /// Returns `None` if `p` is outside the framebuffer bounding box.
-    // TODO: Optimise implementation by moving away from `ImageRaw::pixel` and directly calculating skip value.
     fn pixel(&self, p: Point) -> Option<C> {
         self.as_image().pixel(p)
     }
