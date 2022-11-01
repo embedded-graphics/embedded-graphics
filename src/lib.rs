60cd342ffb2a169dc57441eee8b8c603f108ee87
Jonathan <jounathaen@mail.de>
2022-11-01 22:58:48 +0100

    Added embedded-graphics-framebuffer to external crate list

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,7 @@
 //! * [Virtual seven-segment displays - `eg-seven-segment`](https://crates.io/crates/eg-seven-segment)
 //! * [Canvas for drawing onto before drawing on the display - `embedded-canvas`](https://crates.io/crates/embedded-canvas)
 //! * [Frames Per Second counter for embedded devices - `embedded-fps`](https://crates.io/crates/embedded-fps)
+//! * [Framebuffer with DMA support - `embedded-graphics-framebuf`](https://crates.io/crates/embedded-graphics-framebuf)
 //!
 //! Note that some of these crates may not support the latest version of embedded-graphics.
 //!
